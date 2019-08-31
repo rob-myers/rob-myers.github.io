52e7257e7a89544b7f299819b2966b330702fdda
rob-myers <me.robmyers@gmail.com>
2019-08-31 16:30:47 +0100

    Configuring npm script 'deploy' i.e. use gh-pages to build then copy folder to master branch

---
+++ config/gh-pages.js
@@ -0,0 +1,33 @@
+const ghpages = require('gh-pages');
+
+/**
+ * This task pushes to the `master` branch of the configured `repo`.
+ */
+(async () => {
+  console.log(`Pushing "build" to master branch for GitHub Pages...`);
+
+  try {
+    await new Promise(
+      resolve => {
+        ghpages.publish('../build', {
+          branch: 'master',
+          repo: 'git@github.com:rob-myers/rob-myers.github.io',
+          user: {
+            name: 'Rob Myers',
+            email: 'me.robmyers@gmail.com'
+          }
+        },
+          resolve
+        );
+      },
+    );
+    console.log("Finished.");
+
+  } catch (e) {
+    console.log(`Error: ${e}`);
+    console.error(e);
+  }
+
+
+})()
+
