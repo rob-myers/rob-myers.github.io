c8e97b368c754353436f98da808f3633eb26df9f
Robert S.R. Myers <me.robmyers@gmail.com>
2019-09-29 16:49:03 +0100

    Now using yarn. Clarified that withCSS needed for gitalk module's css.

--- next.config.js
+++ next.config.js
@@ -1,31 +1,40 @@
 const path = require('path')
 
-// https://github.com/zeit/next-plugins/tree/master/packages/next-css
+/**
+ * Currently needed to load gitalk module's gitalk.css.
+ * https://github.com/zeit/next-plugins/tree/master/packages/next-css
+ */
 const withCSS = require('@zeit/next-css');
 
-// https://github.com/twopluszero/next-images
+/**
+ * Used to load pngs.
+ * https://github.com/twopluszero/next-images
+ */
 const withImages = require('next-images');
 
-module.exports = withImages(withCSS({
-  webpack(config, _options) {
-    /**
-     * Add path aliases.
-     * Also defined in tsconfig.json.
-     */
-    Object.assign(config.resolve.alias, {
-      '@components': path.join(__dirname, 'components'),
-      '@model': path.join(__dirname, 'model'),
-      '@store': path.join(__dirname, 'store'),
-      '@service': path.join(__dirname, 'service'),
-      '@assets': path.join(__dirname, 'assets'),
-      '@env': path.join(__dirname, 'env'),
-    });
-    return config;
-  },
+module.exports =
+  withImages(
+  withCSS({
+    webpack(config, _options) {
+      /**
+       * Add path aliases.
+       * Also defined in tsconfig.json.
+       */
+      Object.assign(config.resolve.alias, {
+        '@components': path.join(__dirname, 'components'),
+        '@model': path.join(__dirname, 'model'),
+        '@store': path.join(__dirname, 'store'),
+        '@service': path.join(__dirname, 'service'),
+        '@assets': path.join(__dirname, 'assets'),
+        '@env': path.join(__dirname, 'env'),
+      });
+      return config;
+    },
 
-  exportPathMap: function () {
-    return {
-      '/': { page: '/' }
-    };
+    exportPathMap: function () {
+      return {
+        '/': { page: '/' }
+      };
+    }
   }
-}))
+))
