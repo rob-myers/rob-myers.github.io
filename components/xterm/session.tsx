e2efbf84b0294fa9a8e7121b65d6740aa866117e
Robert S.R. Myers <me.robmyers@gmail.com>
2020-03-07 08:20:36 +0000

    basic css for session component

--- components/xterm/session.tsx
+++ components/xterm/session.tsx
@@ -4,6 +4,7 @@ import dynamic from 'next/dynamic';
 import { Thunk } from '@store/xterm.duck';
 import { redact } from '@model/redux.model';
 import { computeXtermKey } from '@model/xterm/xterm.model';
+import css from './session.scss';
 
 import XTermComponent from './xterm';
 const XTermWithoutSsr: typeof XTermComponent = dynamic(
@@ -25,25 +26,36 @@ export const Session: React.FC<Props> = ({ uid, userKey }) => {
   }, []);
   
   return (
-    <XTermWithoutSsr
-      onMount={(xterm) => {
-        dispatch(Thunk.createSession({
-          uiKey: uid,
-          userKey,
-          xterm: redact(xterm),
-          onCreate: (sessionKey) => sessionKeyRef.current = sessionKey,
-        }));
-      }}
-      options={{
-        fontSize: 12,
-        cursorBlink: true,
-        rendererType: 'dom',
-        theme: {
-          background: 'black',
-          foreground: '#41FF00',
-        },
-      }}
-    />
+    <div>
+      <div className={css.topPanel}>
+        <a
+          className={css.saveButton}
+          onClick={() => dispatch(Thunk.saveOs({}))}
+        >
+          save
+        </a>
+      </div>
+      <XTermWithoutSsr
+        className={css.terminal}
+        onMount={async (xterm) => {
+          dispatch(Thunk.createSession({
+            uiKey: uid,
+            userKey,
+            xterm: redact(xterm),
+            onCreate: (sessionKey) => sessionKeyRef.current = sessionKey,
+          }));
+        }}
+        options={{
+          fontSize: 12,
+          cursorBlink: true,
+          rendererType: 'dom',
+          theme: {
+            background: 'black',
+            foreground: '#41FF00',
+          },
+        }}
+      />
+    </div>
   );
 };
 
