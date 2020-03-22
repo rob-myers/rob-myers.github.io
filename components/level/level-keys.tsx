705dc06d76289d05aada1ed05859263d779b70b1
Robert S.R. Myers <me.robmyers@gmail.com>
2020-03-22 16:10:17 +0000

    cleanup LevelKeys

--- components/level/level-keys.tsx
+++ components/level/level-keys.tsx
@@ -1,5 +1,4 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { ToastProvider } from 'react-toast-notifications';
 
 import { Act } from '@store/level.duck';
 import css from './level.scss';
@@ -24,9 +23,7 @@ const LevelKeys: React.FC<Props> = ({ levelUid, children}) => {
       }}
       tabIndex={0}
     >
-      <ToastProvider>
-        {children}
-      </ToastProvider>
+      {children}
     </div>
   );
 };
