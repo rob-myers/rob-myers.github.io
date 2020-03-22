9466af946d7f845f84beda850c0012e0337eacfc
Robert S.R. Myers <me.robmyers@gmail.com>
2020-03-22 16:09:47 +0000

    cleanup LevelContent

--- components/level/level-content.tsx
+++ components/level/level-content.tsx
@@ -2,20 +2,16 @@ import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 import { subscribeToWorker } from '@model/level/level.worker.model';
 import { Poly2 } from '@model/poly2.model';
-import { NavGraph } from '@model/nav/nav-graph.model';
-import { Vector2, Vector2Json } from '@model/vec2.model';
+import { Vector2Json } from '@model/vec2.model';
 import css from './level.scss';
 
-const LevelContent: React.FC<Props> = ({ levelUid, showNavGraph = false }) => {
+const LevelContent: React.FC<Props> = ({ levelUid }) => {
   const worker = useSelector(({ level: { worker } }) => worker)!;
 
   const [tileFloors, setTileFloors] = useState([] as string[]);
   const [walls, setWalls] = useState([] as [Vector2Json, Vector2Json][]);
   const [floors, setFloors] = useState([] as string[]);
   const [triangles, setTriangles] = useState([] as string[]);
-  // debug
-  const [centers, setCenters] = useState([] as Vector2[]);
-  const [segs, setSegs] = useState([] as Vector2[][]);
 
   useEffect(() => {
     const sub = subscribeToWorker(worker, (msg) => {
@@ -36,15 +32,6 @@ const LevelContent: React.FC<Props> = ({ levelUid, showNavGraph = false }) => {
         case 'send-level-tris': {
           setTriangles(msg.tris.map(x => Poly2.fromJson(x).svgPath));
           break;
-        }
-        case 'send-nav-graph': {// Debug
-          if (showNavGraph) {
-            const navGraph = NavGraph.fromJson(msg.navGraph);
-            const floors = msg.floors.map(floor => Poly2.fromJson(floor));
-            const { centers, segs } = navGraph.dualGraph(floors);
-            setCenters(centers);
-            setSegs(segs);
-          }
         }
       }
     });
@@ -74,32 +61,12 @@ const LevelContent: React.FC<Props> = ({ levelUid, showNavGraph = false }) => {
           <path key={i} d={pathDef} />
         ))}
       </g>
-      {showNavGraph && (// TODO move to LevelMetas
-        <g>
-          {centers.map(({ x, y }, i) => (
-            <circle
-              key={i}
-              cx={x} cy={y} r={2}
-              className={css.svgNavNode}
-            />
-          ))}
-          {segs.map(([ src, dst ], i) => (
-            <line
-              key={i}
-              x1={src.x} y1={src.y}
-              x2={dst.x} y2={dst.y}
-              className={css.svgNavEdge}
-            />
-          ))}
-        </g>
-      )}
     </>
   );
 };
 
 interface Props {
   levelUid: string;
-  showNavGraph?: boolean;
 }
 
 export default LevelContent;
