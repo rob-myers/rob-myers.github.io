1e26b870b1c337262744ef44666b386a3256fad1
Robert S.R. Myers <me.robmyers@gmail.com>
2020-04-17 18:32:24 +0100

    remove old icons; added new icon 'door-1'

--- model/icon/icon.model.ts
+++ model/icon/icon.model.ts
@@ -2,16 +2,7 @@ import { Rect2 } from '@model/rect2.model';
 import { KeyedLookup } from '@model/generic.model';
 
 export type IconType = (
-  | 'briefcase'
-  | 'computer'
-  | 'document-1'
-  | 'document-2'
-  | 'document-3'
-  | 'notebook'
-  | 'smartphone'
-  | 'smartphone-1'
-  | 'user-2'
-  | 'user-3'
+  | 'door-1'
 );
 
 interface IconMeta {
@@ -26,64 +17,10 @@ interface IconMeta {
 }
 
 export const iconLookup: KeyedLookup<IconMeta, IconType> = {
-  /**
-   * 1
-   */
-  'briefcase': {
-    key: 'briefcase',
-    svg: require('./1/briefcase.svg'),
-    srcRect: new Rect2(0, 0, 60, 60),
-    dstRect: new Rect2(0, 0, 3, 3),
-  },
-  'notebook': {
-    key: 'notebook',
-    svg: require('./1/notebook.svg'),
-    srcRect: new Rect2(0, 0, 60, 60),
-    dstRect: new Rect2(0, 0, 3, 3),
-  },
-  'smartphone-1': {
-    key: 'smartphone-1',
-    srcRect: new Rect2(0, 0, 60, 60),
-    dstRect: new Rect2(0, 0, 3, 3),
-    svg: require('./1/smartphone-1.svg'),
-  },
-  /**
-   * 2
-   */
-  'computer': {
-    key: 'computer',
-    svg: require('./2/computer.svg'),
-    srcRect: new Rect2(0, 0, 300, 300),
-    dstRect: new Rect2(0, 0, 3, 3),
-  },
-  'document-2': {
-    key: 'document-2',
-    svg: require('./2/document-2.svg'),
-    srcRect: new Rect2(0, 0, 300, 300),
-    dstRect: new Rect2(0, 0, 3, 3),
-  },
-  'document-3': {
-    key: 'document-3',
-    svg: require('./2/document-3.svg'),
-    srcRect: new Rect2(0, 0, 300, 300),
-    dstRect: new Rect2(0, 0, 3, 3),
-  },
-  'smartphone': {
-    key: 'smartphone',
-    svg: require('./2/smartphone.svg'),
-    srcRect: new Rect2(0, 0, 300, 300),
-    dstRect: new Rect2(0, 0, 3, 3),
-  },
-  'user-2': {
-    key: 'user-2',
-    svg: require('./2/user-2.svg'),
-    srcRect: new Rect2(0, 0, 300, 300),
-    dstRect: new Rect2(0, 0, 3, 3),
-  },
-  'user-3': {
-    key: 'user-3',
-    svg: require('./2/user-3.svg'),
-    srcRect: new Rect2(0, 0, 300, 300),
+  'door-1': {
+    key: 'door-1',
+    svg: require('./door-1.svg'),
+    srcRect: new Rect2(0, 0, 100, 100),
     dstRect: new Rect2(0, 0, 3, 3),
   },
 };
