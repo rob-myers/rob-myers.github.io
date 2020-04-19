186350578222208c75454b76efc467042b303168
Robert S.R. Myers <me.robmyers@gmail.com>
2020-04-19 13:59:08 +0100

    LevelMetaMenu shows meta tags

--- components/level/level-meta-menu.tsx
+++ components/level/level-meta-menu.tsx
@@ -1,19 +1,41 @@
 import { useState, useEffect } from 'react';
 import classNames from 'classnames';
 import { useSelector, useDispatch } from 'react-redux';
+import { LevelMetaGroup } from '@model/level/level-meta.model';
+import { subscribeToWorker } from '@model/level/level.worker.model';
+import { Thunk, Act } from '@store/level.duck';
 import css from './level.scss';
-import { LevelMetaGroupUi } from '@model/level/level-meta.model';
-import { Thunk } from '@store/level.duck';
+import { LevelState } from '@model/level/level.model';
+
+type MetaLookup = LevelState['metaGroups'];
 
 const LevelMetaMenu: React.FC<Props> = ({ levelUid }) => {
-  const [openMetas, setOpenMetas] = useState<LevelMetaGroupUi[]>([]);
+  const [openMetas, setOpenMetas] = useState<LevelMetaGroup[]>([]);
+  const [toGroup, setGroups] = useState<MetaLookup>({});
+
   const metaGroupUi = useSelector(({ level: { instance } }) => instance[levelUid].metaGroupUi);
+  const worker = useSelector(({ level: { worker } }) => worker)!;
+
   const dispatch = useDispatch();
   const closeMenu = () => dispatch(Thunk.closeAllMetas({levelUid}));
 
   useEffect(() => {
-    const groups = Object.values(metaGroupUi);
-    const openMetas = groups.filter(({ open }) => open);
+    const sub = subscribeToWorker(worker, (msg) => {
+      if (msg.key === 'send-level-metas' && msg.levelUid === levelUid) {
+        const metas = msg.metas.map(p => LevelMetaGroup.from(p))
+          .reduce<MetaLookup>((agg, item) => ({ ...agg, [item.key]: item }), {}); 
+        setGroups(metas);
+        dispatch(Act.syncMetaUi(levelUid, Object.values(metas)));
+      }
+    });
+    worker.postMessage({ key: 'request-level-metas', levelUid });
+    return () => sub.unsubscribe();
+  }, []);
+
+  useEffect(() => {
+    const openUis = Object.values(metaGroupUi).filter(({ open }) => open);
+    const openMetas = openUis.map(({ key }) => toGroup[key]).filter(Boolean);
+    // setOpenUis(openUis);
     setOpenMetas(openMetas);
   }, [metaGroupUi]);
 
@@ -24,6 +46,13 @@ const LevelMetaMenu: React.FC<Props> = ({ levelUid }) => {
         style={{ height: openMetas.length * 20 }}
       >
         <section className={css.mainMenu}>
+          {openMetas.map(({ key, metas, metaIndex }) => (
+            <section key={key} className={css.meta}>
+              {
+                metas.filter((_, i) => i === metaIndex).map(meta => meta.tags)
+              }
+            </section>
+          ))}
         </section>
         <section className={css.rightMenu}>
           <button onClick={closeMenu}>
