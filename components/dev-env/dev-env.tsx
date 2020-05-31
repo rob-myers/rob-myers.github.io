63d36d138c922da5c1de6f6bea2442e307bd3f10
Robert S.R. Myers <me.robmyers@gmail.com>
2020-05-31 18:11:32 +0100

    update styling of DevEnv

--- components/dev-env/dev-env.tsx
+++ components/dev-env/dev-env.tsx
@@ -1,24 +1,25 @@
 import { useState, useCallback, useMemo } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import { useDispatch } from 'react-redux';
+
 import { exampleScss1 } from '@model/code/examples';
-import { TranspiledCode, FileType, permute, tsxEditorProps } from '@model/monaco/monaco.model';
+import { TranspiledCode, FileType, tsxEditorProps } from '@model/monaco/monaco.model';
 import { Thunk } from '@store/worker.duck';
 import TsxEditor from '@components/monaco/tsx-editor';
 import Editor from '@components/monaco/editor';
 import EsModule from './es-module';
 import css from './dev-env.scss';
 
-// Used for manual syntax highlighting, so mustn't use CSS modules.
-// See `styles.config.ts`. Using `require` prevents tree-shaking.
+// Used to fix JSX syntax highlighting of monaco editor.
+// We mustn't use CSS modules (see `styles.config.ts`).
+// Using `require` prevents tree-shaking.
 require('./monaco-override.scss');
 
 const DevEnv: React.FC<Props> = ({ uid }) => {
   const [editing, setEditing] = useState<FileType>('tsx');
   const [code, setCode] = useState('');
   const [codeEsmUrl, setCodeEsmUrl] = useState('');
+  // const ready = useSelector(({ worker: { monacoTypesLoaded } }) => !!monacoTypesLoaded);
   
-  const ready = useSelector(({ worker: { monacoTypesLoaded } }) => !!monacoTypesLoaded);
-  const toggle = useCallback(() => ready && setEditing(permute(editing)), [ready, editing]);
   
   const dispatch = useDispatch();
   const onTranspile = useCallback((result: TranspiledCode) => {
@@ -36,44 +37,47 @@ const DevEnv: React.FC<Props> = ({ uid }) => {
   return (
     <section className={css.root}>
       <div className={css.header}>
-        <a onClick={toggle}>{permute(editing)}</a>
+        <a onClick={() => setEditing('tsx')}>tsx</a>
+        <a onClick={() => setEditing('scss')}>scss</a>
       </div>
-      <div className={css.editor}>
-        {editing === 'tsx' && (
-          <TsxEditor
-            editorKey={`${uid}-tsx-editor`}
-            modelKey={`${uid}-model/tsx`}
-            editorProps={tsxEditorProps}
-            onTranspile={onTranspile}
-          />
-        )}
-        {editing === 'scss' && (
-          <Editor
-            editorKey={`${uid}-sass-editor`}
-            modelKey={`${uid}-model/scss`}
-            filename="file:///main.scss"
-            width={'100%'}
-            code={exampleScss1}
-          />
-        )}
-      </div>
-      <div id={`${uid}-render-root`}>
-        {code && (
-          <EsModule
-            code={code}
-            scriptId={`${uid}-transpiled-tsx`}
-            onMount={(scriptSrcUrl) => {
-              // console.log({ scriptSrcUrl });
-              setCodeEsmUrl(scriptSrcUrl);
-            }}
-          />
-        )}
-        {codeEsmUrl && (
-          <EsModule
-            code={bootstrapCode}
-            scriptId={`${uid}-bootstrap-app`}
-          />
-        )}        
+      <div className={css.editorAndView}>
+        <div className={css.editor}>
+          {editing === 'tsx' && (
+            <TsxEditor
+              editorKey={`${uid}-tsx-editor`}
+              modelKey={`${uid}-tsx-model`}
+              editorProps={tsxEditorProps}
+              onTranspile={onTranspile}
+            />
+          )}
+          {editing === 'scss' && (
+            <Editor
+              editorKey={`${uid}-sass-editor`}
+              modelKey={`${uid}-sass-model`}
+              filename="file:///main.scss"
+              width={'100%'}
+              code={exampleScss1}
+            />
+          )}
+        </div>
+        <div id={`${uid}-render-root`} className={css.view}>
+          {code && (
+            <EsModule
+              code={code}
+              scriptId={`${uid}-transpiled-tsx`}
+              onMount={(scriptSrcUrl) => {
+                // console.log({ scriptSrcUrl });
+                setCodeEsmUrl(scriptSrcUrl);
+              }}
+            />
+          )}
+          {codeEsmUrl && (
+            <EsModule
+              code={bootstrapCode}
+              scriptId={`${uid}-bootstrap-app`}
+            />
+          )}        
+        </div>
       </div>
     </section>
   );
