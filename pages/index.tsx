f1c5789e66042dd2643ec3f4fa11e152ceb014ca
Robert S.R. Myers <me.robmyers@gmail.com>
2020-07-03 17:04:54 +0100

    improved menu style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import Layout from '@components/layout/layout';
 import TopMenu from '@components/top-menu/top-menu';
-import Gitalk from '@components/gitalk/gitalk';
 
 const Home: React.FC = () => {
   return (
@@ -9,7 +8,6 @@ const Home: React.FC = () => {
         title="rob-myers"
         label="Robert S. R. Myers"
       />
-      <Gitalk />
     </Layout>  
   );
 };
