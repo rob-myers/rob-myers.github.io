b666824bc8179a091b1b31448757035941ef59d8
Robert S.R. Myers <me.robmyers@gmail.com>
2020-07-08 15:32:31 +0100

    must require es-react-refresh/bootstrap to avoid tree-shake in prod

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '@public/es-react-refresh/bootstrap';
+// require avoids tree-shake in production
+require('@public/es-react-refresh/bootstrap');
 import { NextComponentType, NextPageContext } from 'next';
 import { Router } from 'next/dist/client/router';
 import Head from 'next/head';
