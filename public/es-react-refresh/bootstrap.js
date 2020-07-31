3c0035c1caa04894036218b2e3bc21c07b82dbbf
Robert S.R. Myers <me.robmyers@gmail.com>
2020-07-31 15:05:15 +0100

    next.js react-refresh was not working until added setTimeout for runtime version; will no longer use runtime redux or react-redux

--- public/es-react-refresh/bootstrap.js
+++ public/es-react-refresh/bootstrap.js
@@ -5,12 +5,11 @@ if (typeof window !== 'undefined') {
    * Hook into ReactDOM initialization, see also:
    * @next/react-refresh-utils/runtime.js
    */
-  RefreshRuntime.injectIntoGlobalHook(window);
+  setTimeout(() => RefreshRuntime.injectIntoGlobalHook(window));
 }
 
 import RefreshHelpers from './helpers';
 import React from '../es-react/react';
-import * as Redux from '../es-redux/redux';
 
 import { REFRESH_HELPERS, REFRESH_REG, REFRESH_SIG, LIVE_REACT, LIVE_REDUX, LIVE_REACT_REDUX } from '../constants';
 
@@ -18,10 +17,10 @@ if (typeof window !== 'undefined') {
   window[REFRESH_REG] = function (filename, type, id) {
     RefreshRuntime.register(type, filename + ' ' + id)
   }
-  window[REFRESH_SIG] = RefreshRuntime.createSignatureFunctionForTransform
+  window[REFRESH_SIG] = RefreshRuntime.createSignatureFunctionForTransform;
   
   // Register global helpers (unused?)
-  window[REFRESH_HELPERS] = RefreshHelpers
+  window[REFRESH_HELPERS] = RefreshHelpers;
   
   /**
    * We need the dynamic app module to refer to the same `react`.
@@ -31,15 +30,18 @@ if (typeof window !== 'undefined') {
    */
   window[LIVE_REACT] = React;
 
-  window[LIVE_REDUX] = Redux;
-
   /**
-   * Import dynamically because was silently breaking react-refresh,
-   * presumably via early reference to es-react/react-dom inside
-   * public/es-react-redux/utils/reactBatchedUpdates.js.
+   * TODO provide redux, react-redux from compiled mmodules
    */
-  import('../es-react-redux/index').then((imported) =>
-    window[LIVE_REACT_REDUX] = imported);
+  // window[LIVE_REDUX] = Redux;
+
+  // /**
+  //  * Import dynamically because was silently breaking react-refresh,
+  //  * presumably via early reference to es-react/react-dom inside
+  //  * public/es-react-redux/utils/reactBatchedUpdates.js.
+  //  */
+  // import('../es-react-redux/index').then((imported) =>
+  //   window[LIVE_REACT_REDUX] = imported);
 }
 
 export default function preventTreeShake() {}
