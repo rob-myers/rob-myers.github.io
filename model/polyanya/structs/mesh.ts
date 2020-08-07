ec732519e10c864bf20a00acd9e4c34e72be86ed
Robert S.R. Myers <me.robmyers@gmail.com>
2020-08-07 08:26:00 +0100

    cleanup polyanya mesh

--- model/polyanya/structs/mesh.ts
+++ model/polyanya/structs/mesh.ts
@@ -28,38 +28,38 @@ export class PolyContainment {
 
   toString(): string {
     switch (this.type) {
-        case PolyContainmentType.OUTSIDE:
-          return 'OUTSIDE';
-        case PolyContainmentType.INSIDE:
-          return 'INSIDE';
-        case PolyContainmentType.ON_EDGE:
-          return `ON_EDGE (poly ${this.adjacent_poly}, vertices ${this.vertex1}, ${this.vertex2})`;
-        case PolyContainmentType.ON_VERTEX:
-          return `ON_VERTEX (${this.vertex1})`;
-        default:
-          throw testNever(this.type);
+      case PolyContainmentType.OUTSIDE:
+        return 'OUTSIDE';
+      case PolyContainmentType.INSIDE:
+        return 'INSIDE';
+      case PolyContainmentType.ON_EDGE:
+        return `ON_EDGE (poly ${this.adjacent_poly}, vertices ${this.vertex1}, ${this.vertex2})`;
+      case PolyContainmentType.ON_VERTEX:
+        return `ON_VERTEX (${this.vertex1})`;
+      default:
+        throw testNever(this.type);
     }
   }
 };
 
 export enum PointLocationType {
-    // Does not use any ints.
-    NOT_ON_MESH,
-    // Uses poly1 (the polygon it is on).
-    IN_POLYGON,
-    // Uses poly1 (the polygon it is on) and both vertices.
-    ON_MESH_BORDER,       // edge: a polygon is not traversable
-    // Uses poly1, poly2 and both vertices.
-    ON_EDGE,              // edge: both polygons are traversable
-    // Uses vertex1.
-    // Can use poly1 to specify the "grid corrected poly".
-    // Will need to manually assign poly1, though.
-    ON_CORNER_VERTEX_AMBIG,   // vertex; two+ polygons are not traversable
-    // Uses vertex1. Also returns an arbirary traversable adjacent
-    // polygon in poly1.
-    ON_CORNER_VERTEX_UNAMBIG, // vertex; one polygon is not traversable
-    // Uses vertex1. Also returns an arbitrary adjacent polygon in poly1.
-    ON_NON_CORNER_VERTEX, // vertex: all polygons are traversable
+  // Does not use any ints.
+  NOT_ON_MESH,
+  // Uses poly1 (the polygon it is on).
+  IN_POLYGON,
+  // Uses poly1 (the polygon it is on) and both vertices.
+  ON_MESH_BORDER,       // edge: a polygon is not traversable
+  // Uses poly1, poly2 and both vertices.
+  ON_EDGE,              // edge: both polygons are traversable
+  // Uses vertex1.
+  // Can use poly1 to specify the "grid corrected poly".
+  // Will need to manually assign poly1, though.
+  ON_CORNER_VERTEX_AMBIG,   // vertex; two+ polygons are not traversable
+  // Uses vertex1. Also returns an arbirary traversable adjacent
+  // polygon in poly1.
+  ON_CORNER_VERTEX_UNAMBIG, // vertex; one polygon is not traversable
+  // Uses vertex1. Also returns an arbitrary adjacent polygon in poly1.
+  ON_NON_CORNER_VERTEX, // vertex: all polygons are traversable
 };
 
 class PointLocation {
@@ -76,22 +76,22 @@ class PointLocation {
 
   public toString() {
     switch (this.type) {
-        case PointLocationType.NOT_ON_MESH:
-            return 'NOT_ON_MESH';
-        case PointLocationType.IN_POLYGON:
-            return `IN_POLYGON (${this.poly1})`;
-        case PointLocationType.ON_MESH_BORDER:
-            return `ON_MESH_BORDER (poly ${this.poly1}, vertices ${this.vertex1}, ${this.vertex2})`;
-        case PointLocationType.ON_EDGE:
-            return `ON_EDGE (polys ${this.poly1}, ${this.poly2}, vertices ${this.vertex1}, ${this.vertex2});`;
-        case PointLocationType.ON_CORNER_VERTEX_AMBIG:
-            return `ON_CORNER_VERTEX_AMBIG (${this.vertex1}, poly? ${this.poly1})`;
-        case PointLocationType.ON_CORNER_VERTEX_UNAMBIG:
-            return `ON_CORNER_VERTEX_UNAMBIG (${this.vertex1}, poly ${this.poly1})`;
-        case PointLocationType.ON_NON_CORNER_VERTEX:
-            return `ON_NON_CORNER_VERTEX (${this.vertex1}, poly ${this.poly1})`;
-        default:
-            throw testNever(this.type);
+      case PointLocationType.NOT_ON_MESH:
+        return 'NOT_ON_MESH';
+      case PointLocationType.IN_POLYGON:
+        return `IN_POLYGON (${this.poly1})`;
+      case PointLocationType.ON_MESH_BORDER:
+        return `ON_MESH_BORDER (poly ${this.poly1}, vertices ${this.vertex1}, ${this.vertex2})`;
+      case PointLocationType.ON_EDGE:
+        return `ON_EDGE (polys ${this.poly1}, ${this.poly2}, vertices ${this.vertex1}, ${this.vertex2});`;
+      case PointLocationType.ON_CORNER_VERTEX_AMBIG:
+        return `ON_CORNER_VERTEX_AMBIG (${this.vertex1}, poly? ${this.poly1})`;
+      case PointLocationType.ON_CORNER_VERTEX_UNAMBIG:
+        return `ON_CORNER_VERTEX_UNAMBIG (${this.vertex1}, poly ${this.poly1})`;
+      case PointLocationType.ON_NON_CORNER_VERTEX:
+        return `ON_NON_CORNER_VERTEX (${this.vertex1}, poly ${this.poly1})`;
+      default:
+        throw testNever(this.type);
     }
   }
 
@@ -147,13 +147,21 @@ class PointLocation {
 
 /** Index of first key `k` not smaller than `key`. */
 function lowerBoundIndex<K extends number, V>(map: Map<K, V>, key: K): number | null {
-  const found = [...map.keys()].findIndex(k => k >= key);
-  return found === -1 ? null : found;
+  let i = 0;
+  for (const k of map.keys()) {
+    if (k >= key) return i;
+    i++;
+  }
+  return null;
 }
 /** Index of first key `k` greater than `key`. */
 function upperBoundIndex<K extends number, V>(map: Map<K, V>, key: K): number | null {
-  const found = [...map.keys()].findIndex(k => k > key);
-  return found === -1 ? null : found;
+  let i = 0;
+  for (const k of map.keys()) {
+    if (k > key) return i;
+    i++;
+  }
+  return null;
 }
 function upperBound<K extends number, V>(map: Map<K, V>, key: K): K | null {
   for (const k of map.keys()) {
@@ -170,12 +178,10 @@ export default class Mesh {
   private max_x = Number.MIN_SAFE_INTEGER;
   private max_y = Number.MIN_SAFE_INTEGER;
   
-  mesh_vertices!: Vertex[];
-  mesh_polygons!: Polygon[];
+  mesh_vertices: Vertex[];
+  mesh_polygons: Polygon[];
   max_poly_sides: number;
 
-  // Mesh(std::istream& infile);
-  // void read(std::istream& infile);
   constructor({ vertices, polygons, vertexToPolys}: MeshJson) {
     this.max_poly_sides = 0;
 
@@ -213,28 +219,17 @@ export default class Mesh {
     this.precalc_point_location();
   }
 
-  // void precalc_point_location();
   precalc_point_location() {
-    // Ensure the iterator `this.slabs` is aligned to key-ordering
+    // Ensure iterator `this.slabs` is aligned to key-ordering
     const pairs = this.mesh_vertices.map(v => [v.p.x, []] as [number, number[]]);
     pairs.sort((a, b) => a[0] < b[0] ? -1 : 1);
     this.slabs = new Map(pairs);
     
-    // Correct below?
-    for (let i = 0; i < this.mesh_polygons.length; i++) {
-      const p = this.mesh_polygons[i];
-      // console.log({ p });
-      const lowIndex = lowerBoundIndex(this.slabs, p.min_x);
-      const highIndex = upperBoundIndex(this.slabs, p.max_x)??(this.slabs.size - 1);
-      // console.log({ i, low_it: lowIndex, high_it: highIndex });
-
-      if (lowIndex === null) {
-        continue;
-      }
-      const pairs = [...this.slabs.entries()]; 
-      for (let it = lowIndex; it <= highIndex; it++) {
-        pairs[it][1].push(i);
-      }
+    for (const [i, p] of this.mesh_polygons.entries()) {
+      const lowJ = lowerBoundIndex(this.slabs, p.min_x)!;
+      const highJ = upperBoundIndex(this.slabs, p.max_x)??(this.slabs.size - 1);
+      let j = -1;
+      this.slabs.forEach((slab) => lowJ <= ++j && j <= highJ && slab.push(i));
     }
     for (const [, inner] of this.slabs) {
       inner.sort((a: number, b: number) => {
@@ -242,11 +237,11 @@ export default class Mesh {
         // If tied, sort based on width of poly.
         const ap = this.mesh_polygons[a];
         const bp = this.mesh_polygons[b];
-        const as = ap.min_y + ap.max_y, bs = bp.min_y + ap.max_y;
-        if (as === bs) {
-          return (ap.max_y - ap.min_y) > (bp.max_y - bp.min_y) ? -1 : 1;
-        }
-        return as < bs ? -1 : 1;
+        const as = ap.min_y + ap.max_y;
+        const bs = bp.min_y + ap.max_y;
+        return as === bs
+          ? (ap.max_y - ap.min_y) > (bp.max_y - bp.min_y) ? -1 : 1
+          : as < bs ? -1 : 1;
       });
     }
   }
@@ -288,7 +283,6 @@ export default class Mesh {
     let i = closeIndex;
     let next_delta = 1;
     let walk_delta = 0; // way to go when walking normally
-
     
     // console.log({ i, ps });
     while (i >= 0 && i < ps) {
@@ -368,43 +362,43 @@ export default class Mesh {
     for (let polygon = 0; polygon < this.mesh_polygons.length; polygon++) {
       const result = this.poly_contains_point(polygon, p);
       switch (result.type) {
-          case PolyContainmentType.OUTSIDE:
-            // Does not contain: try the next one.
-            break;
-          case PolyContainmentType.INSIDE:
-            // This one strictly contains the point.
-            return new PointLocation(PointLocationType.IN_POLYGON, polygon, -1, -1, -1);
-          case PolyContainmentType.ON_EDGE:
-              // This one lies on the edge.
-              // Chek whether the other one is -1.
+        case PolyContainmentType.OUTSIDE:
+          // Does not contain: try the next one.
+          break;
+        case PolyContainmentType.INSIDE:
+          // This one strictly contains the point.
+          return new PointLocation(PointLocationType.IN_POLYGON, polygon, -1, -1, -1);
+        case PolyContainmentType.ON_EDGE:
+          // This one lies on the edge.
+          // Chek whether the other one is -1.
+          return new PointLocation(
+            (result.adjacent_poly == -1
+              ? PointLocationType.ON_MESH_BORDER
+              : PointLocationType.ON_EDGE),
+            polygon, result.adjacent_poly,
+            result.vertex1, result.vertex2
+          );
+        case PolyContainmentType.ON_VERTEX: {
+          // This one lies on a corner.
+          const v = this.mesh_vertices[result.vertex1];
+          if (v.is_corner) {
+            if (v.is_ambig) {
               return new PointLocation(
-                (result.adjacent_poly == -1
-                  ? PointLocationType.ON_MESH_BORDER
-                  : PointLocationType.ON_EDGE),
-                polygon, result.adjacent_poly,
-                result.vertex1, result.vertex2
+                PointLocationType.ON_CORNER_VERTEX_AMBIG, -1, -1, result.vertex1, -1,
               );
-          case PolyContainmentType.ON_VERTEX: {
-            // This one lies on a corner.
-            const v = this.mesh_vertices[result.vertex1];
-            if (v.is_corner) {
-                if (v.is_ambig) {
-                  return new PointLocation(
-                    PointLocationType.ON_CORNER_VERTEX_AMBIG, -1, -1, result.vertex1, -1,
-                  );
-                } else {
-                  return new PointLocation(
-                    PointLocationType.ON_CORNER_VERTEX_UNAMBIG, polygon, -1, result.vertex1, -1,
-                  );
-                }
             } else {
               return new PointLocation(
-                PointLocationType.ON_NON_CORNER_VERTEX, polygon, -1, result.vertex1, -1,
+                PointLocationType.ON_CORNER_VERTEX_UNAMBIG, polygon, -1, result.vertex1, -1,
               );
             }
+          } else {
+            return new PointLocation(
+              PointLocationType.ON_NON_CORNER_VERTEX, polygon, -1, result.vertex1, -1,
+            );
           }
-          default:
-            throw testNever(result.type);
+        }
+        default:
+          throw testNever(result.type);
       }
     }
     // Haven't returned yet, therefore P does not lie on the mesh.
@@ -414,85 +408,82 @@ export default class Mesh {
   // PolyContainment poly_contains_point(int poly, Point& p);
   // Finds out whether the polygon specified by "poly" contains point P.
   poly_contains_point(poly: number, p: Point): PolyContainment {
-      // The below is taken from
-      // "An Efficient Test for a Point to Be in a Convex Polygon"
-      // from the Wolfram Demonstrations Project
-      // demonstrations.wolfram.com/AnEfficientTestForAPointToBeInAConvexPolygon/
+    // The below is taken from
+    // "An Efficient Test for a Point to Be in a Convex Polygon"
+    // from the Wolfram Demonstrations Project
+    // demonstrations.wolfram.com/AnEfficientTestForAPointToBeInAConvexPolygon/
 
-      // Assume points are in counterclockwise order.
-      const poly_ref = this.mesh_polygons[poly];
-      if (
-        p.x < poly_ref.min_x - EPSILON
-        || p.x > poly_ref.max_x + EPSILON
-        || p.y < poly_ref.min_y - EPSILON
-        || p.y > poly_ref.max_y + EPSILON
-      ) {
-        return new PolyContainment(PolyContainmentType.OUTSIDE, -1, -1, -1);
+    // Assume points are in counterclockwise order.
+    const poly_ref = this.mesh_polygons[poly];
+    if (
+      p.x < poly_ref.min_x - EPSILON
+      || p.x > poly_ref.max_x + EPSILON
+      || p.y < poly_ref.min_y - EPSILON
+      || p.y > poly_ref.max_y + EPSILON
+    ) {
+      return new PolyContainment(PolyContainmentType.OUTSIDE, -1, -1, -1);
+    }
+    const last_point_in_poly = this.mesh_vertices[lastEl(poly_ref.vertices)!].p;
+    const ZERO = new Point(0, 0);
+
+    let last = last_point_in_poly.sub(p);
+    if (last.equals(ZERO)) {
+      return new PolyContainment(PolyContainmentType.ON_VERTEX, -1, lastEl(poly_ref.vertices)!, -1);
+    }
+
+    let last_index = lastEl(poly_ref.vertices)!;
+    for (let i = 0; i < poly_ref.vertices.length; i++) {
+      const point_index = poly_ref.vertices[i];
+      const cur = this.mesh_vertices[point_index].p.sub(p);
+      if (cur.equals(ZERO)) {
+        return new PolyContainment(PolyContainmentType.ON_VERTEX, -1, point_index, -1);
       }
-      const last_point_in_poly = this.mesh_vertices[lastEl(poly_ref.vertices)!].p;
-      const ZERO = new Point(0, 0);
+      const cur_a = last.cross(cur);
+      if (Math.abs(cur_a) < EPSILON) {
+        // The line going from cur to last goes through p.
+        // This means that they are collinear.
+        // The associated polygon should simply be polygons[i] in version
+        // 2 of the file format.
 
-      let last = last_point_in_poly.sub(p);
-      if (last.equals(ZERO)) {
-        return new PolyContainment(PolyContainmentType.ON_VERTEX, -1, lastEl(poly_ref.vertices)!, -1);
+        // Ensure that cur = c*last where c is negative.
+        // If not, this means that the point is either outside or that this
+        // segment is collinear to an adjacent one.
+        if (cur.x) {
+          if (!((cur.x > 0) !== (last.x > 0))) {
+            last = cur;
+            last_index = point_index;
+            continue;
+          }
+        } else {
+          if (!((cur.y > 0) !== (last.y > 0))) {
+            last = cur;
+            last_index = point_index;
+            continue;
+          }
+        }
+        return new PolyContainment(
+          PolyContainmentType.ON_EDGE, poly_ref.polygons[i], point_index, last_index,
+        );
       }
 
-      let last_index = lastEl(poly_ref.vertices)!;
-      for (let i = 0; i < poly_ref.vertices.length; i++) {
-        const point_index = poly_ref.vertices[i];
-        const cur = this.mesh_vertices[point_index].p.sub(p);
-        if (cur.equals(ZERO)) {
-          return new PolyContainment(PolyContainmentType.ON_VERTEX, -1, point_index, -1);
-        }
-        const cur_a = last.cross(cur);
-        if (Math.abs(cur_a) < EPSILON) {
-            // The line going from cur to last goes through p.
-            // This means that they are collinear.
-            // The associated polygon should simply be polygons[i] in version
-            // 2 of the file format.
-
-            // Ensure that cur = c*last where c is negative.
-            // If not, this means that the point is either outside or that this
-            // segment is collinear to an adjacent one.
-            if (cur.x) {
-              if (!((cur.x > 0) !== (last.x > 0))) {
-                last = cur;
-                last_index = point_index;
-                continue;
-              }
-            } else {
-              if (!((cur.y > 0) !== (last.y > 0))) {
-                last = cur;
-                last_index = point_index;
-                continue;
-              }
-            }
-            return new PolyContainment(
-              PolyContainmentType.ON_EDGE, poly_ref.polygons[i], point_index, last_index,
-            );
-        }
-
-        // Because we assume that the points are counterclockwise,
-        // we can immediately terminate when we see a negatively signed area.
-        if (cur_a < 0) {
-          return new PolyContainment(PolyContainmentType.OUTSIDE, -1, -1, -1);
-        }
-        last = cur;
-        last_index = point_index;
+      // Because we assume that the points are counterclockwise,
+      // we can immediately terminate when we see a negatively signed area.
+      if (cur_a < 0) {
+        return new PolyContainment(
+          PolyContainmentType.OUTSIDE, -1, -1, -1);
       }
+      last = cur;
+      last_index = point_index;
+    }
     return new PolyContainment(PolyContainmentType.INSIDE, -1, -1, -1);
   }
 
   // void print(std::ostream& outfile);
   print() {
     return `mesh with ${
-      this.mesh_vertices.length
-    } vertices, ${
-      this.mesh_polygons
-    } polygons\n` +
+      this.mesh_vertices.length} vertices, ${this.mesh_polygons} polygons\n` +
     `vertices:\n${
-      this.mesh_vertices.map(x => `${x.p} ${x.is_corner}\n`).join(' ')
-    }` +
+      this.mesh_vertices.map(x => `${x.p} ${x.is_corner}\n`).join(' ')}` +
     `polygons:\n${
       this.mesh_polygons
         .flatMap(x => x.vertices.map(i => this.mesh_vertices[i].p))
