a9b17354f358205e63584acf87301ff8a3dbde6f
Robert S.R. Myers <me.robmyers@gmail.com>
2020-08-15 15:51:59 +0100

    scroll to initial hash

--- blog/blog.tsx
+++ blog/blog.tsx
@@ -1,8 +1,18 @@
+import { useEffect } from 'react';
 import Blog1 from './1/blog-1.mdx';
 import Blog2 from './2/blog-2.mdx';
+import { getWindow } from '@model/dom.model';
 import css from './blog.scss';
 
 const BlogRoot: React.FC = () => {
+  useEffect(() => {
+    if (getWindow()?.location.hash) {// Scroll to initial hash
+      const found = Array.from(document.getElementsByTagName('a'))
+        .find(el => el.name === window.location.hash.slice(1));
+      found && setTimeout(() => window.scrollTo({ top: found.offsetTop }), 100);
+    }
+  }, []);
+
   return (
     <div className={css.root}>
       <Blog1/>
