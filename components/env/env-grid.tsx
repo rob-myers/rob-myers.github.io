7ede6f40101e7ebc0b5caee12224894c20d2f37f
Robert S.R. Myers <me.robmyers@gmail.com>
2020-08-17 13:03:23 +0100

    now have 2 grids i.e. extra one 5x larger than original

--- components/env/env-grid.tsx
+++ components/env/env-grid.tsx
@@ -6,12 +6,15 @@ import css from './env.scss';
 
 const LevelGrid: React.FC<Props> = ({ envKey }) => {
   const gridId = useRef(`grid-${envKey}`);
+  const largeGridId = useRef(`large-grid-${envKey}`);
   const renderBounds = useSelector(({ env: { instance } }) => instance[envKey].renderBounds);
   const zoomFactor = useSelector(({ env: { instance } }) => instance[envKey].zoom);
 
   // Compute grid pattern offset
   const dx = -posModulo(renderBounds.x, tileDim);
   const dy = -posModulo(renderBounds.y, tileDim);
+  const Dx = -posModulo(renderBounds.x, 5 * tileDim);
+  const Dy = -posModulo(renderBounds.y, 5 * tileDim);
 
   return (
     <>
@@ -24,13 +27,26 @@ const LevelGrid: React.FC<Props> = ({ envKey }) => {
             strokeWidth="0.5"
           />
         </pattern>
+        <pattern id={largeGridId.current} x={Dx} y={Dy} width={5 * tileDim} height={5 * tileDim} patternUnits="userSpaceOnUse">
+          <path
+            className={css.svgGridPath}
+            d={`M ${5 * tileDim} 0 L 0 0 0 ${5 * tileDim}`}
+            fill="none"
+            strokeWidth="0.5"
+          />
+        </pattern>
       </defs>
       <rect
         className={css.svgGrid}
         width={`${100 / zoomFactor}%`}
         height={`${100 / zoomFactor}%`}
         fill={`url(#${gridId.current})`}
-        // fill={`url(${location.href}#small-${gridId.current})`}
+      />
+      <rect
+        className={css.svgGrid}
+        width={`${100 / zoomFactor}%`}
+        height={`${100 / zoomFactor}%`}
+        fill={`url(#${largeGridId.current})`}
       />
     </>
   );
