e270c5a49800443f34d0c2f2f1c6bfe2c4e17314
Robert S.R. Myers <me.robmyers@gmail.com>
2020-08-22 14:06:47 +0100

    move infinite grid to components/three

--- components/demo/three/pan-zoom.tsx
+++ components/demo/three/pan-zoom.tsx
@@ -1,21 +1,16 @@
 import dynamic from 'next/dynamic';
-import * as THREE from 'three';
-import { useRef, useMemo, Suspense } from 'react';
+import { useRef, Suspense } from 'react';
 import { Canvas, extend, useThree, useFrame } from 'react-three-fiber';
 import { PanZoomControls } from '@model/three/controls';
-import { Wall, Table } from './geom';
+// import { Wall, Table } from './geom';
 import css from './three.scss';
 import { PerspectiveCamera } from 'three';
+import Grid from '@components/three/grid';
 
 const First = dynamic(() => import('@components/demo/three/first.gltf'), { ssr: false });
 
 // See types/react-three-fiber/three-types.d.ts
 extend({ PanZoomControls });
-
-type Coord = [number, number, number];
-const _zero: Coord = [0, 0, 0];
-const _one: Coord = [1, 1, 1];
-type El = JSX.IntrinsicElements;
 
 const CameraControls: React.FC = () => {
   const { camera, gl: { domElement } } = useThree();
@@ -25,76 +20,10 @@ const CameraControls: React.FC = () => {
 };
 
 const PanZoom: React.FC = () => {
-  const gridMesh = useRef<El['mesh']>(null);
-  /**
-   * https://github.com/Fyrestar/THREE.InfiniteGridHelper/blob/master/InfiniteGridHelper.js
-   */
-  const gridMaterial = useMemo(() => (
-    <shaderMaterial
-      attach="material"
-      side={THREE.DoubleSide}
-      uniforms={{
-        uSize1: { value: 0.1 },
-        uSize2: { value: 1 },
-        uColor: { value: new THREE.Color('#777') },
-        uDistance: { value: 1024 },
-      }}
-      transparent
-      vertexShader={`
-        varying vec3 worldPosition;
-        uniform float uDistance;
-        
-        void main() {
-            vec3 pos = position.xzy * uDistance;
-            pos.xz += cameraPosition.xz;
-            worldPosition = pos;
-            gl_Position = projectionMatrix * modelViewMatrix * vec4(pos, 1.0);
-        }
-
-      `}
-      fragmentShader={`
-        varying vec3 worldPosition;
-            
-        uniform float uSize1;
-        uniform float uSize2;
-        uniform vec3 uColor;
-        uniform float uDistance;
-        
-        float getGrid(float size) {
-          vec2 r = worldPosition.xz / size;
-          vec2 grid = abs(fract(r - 0.5) - 0.5) / fwidth(r);
-          float line = min(grid.x, grid.y);
-          return 1.0 - min(line, 1.0);
-        }
-        
-        void main() {
-          float d = 1.0 - min(distance(cameraPosition.xz, worldPosition.xz) / uDistance, 1.0);
-          float g1 = getGrid(uSize1);
-          float g2 = getGrid(uSize2);
-          
-          gl_FragColor = vec4(uColor.rgb, mix(g2, g1, g1) * pow(d, 3.0));
-          gl_FragColor.a = mix(0.5 * gl_FragColor.a, gl_FragColor.a, g2);
-          if ( gl_FragColor.a <= 0.0 ) discard;
-        }
-      `}
-      extensions={{
-        derivatives: true,
-      } as any}
-    />
-  ), []);
 
   return (
     <group>
-      <mesh
-        ref={gridMesh}
-        position={[0, 0, 0]}
-        scale={[1, 1, 1]}
-        rotation={[Math.PI/2, 0, 0]}
-        frustumCulled={false}
-      >
-        <planeBufferGeometry args={[2, 2, 1, 1]} attach="geometry" />
-        {gridMaterial}
-      </mesh>
+      <Grid />
       <group rotation={[Math.PI/2, 0, 0]}>
         <Suspense fallback={null}>
           <First />
