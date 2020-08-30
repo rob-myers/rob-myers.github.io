567414598faabc5c0de4efe7a9a24c99411a22d0
Robert S.R. Myers <me.robmyers@gmail.com>
2020-08-30 17:35:46 +0100

    can traverse shell parse tree

---
+++ model/shell/parse.util.ts
@@ -0,0 +1,93 @@
+import { testNever } from "@model/generic.model";
+import { ParsedSh, BaseNode } from "./parse.service";
+
+export function getChildren(node: ParsedSh): ParsedSh[] {
+  switch (node.type) {
+    case 'ArithmCmd': return [node.X];
+    case 'ArithmExp': return [node.X];
+    case 'ArrayElem': return [
+      ...node.Index ? [node.Index] : [],
+      node.Value,
+    ];
+    case 'ArrayExpr': return node.Elems;
+    case 'Assign': return [
+      ...node.Array ? [node.Array] : [],
+      ...node.Index ? [node.Index] : [],
+      ...node.Value ? [node.Value] : [],
+    ];
+    case 'BinaryArithm': 
+    case 'BinaryCmd':
+    case 'BinaryTest': return [node.X, node.Y];
+    case 'Block': return [node.StmtList];
+    case 'CStyleLoop': return [node.Cond, node.Init, node.Post];
+    case  'CallExpr': return ([] as ParsedSh[])
+      .concat(node.Args, node.Assigns);
+    case 'CaseClause': return ([] as ParsedSh[])
+      .concat(node.Items, node.Word);
+    case 'CaseItem': return  ([] as ParsedSh[])
+      .concat(node.Patterns, node.StmtList);
+    case 'CmdSubst': return [node.StmtList];
+    case 'Comment': return [];
+    case 'CoprocClause': return [node.Stmt];
+    case 'DblQuoted': return node.Parts;
+    case 'DeclClause': return ([] as ParsedSh[])
+        .concat(node.Assigns, node.Opts, node.Variant, node.others);
+    case 'ExtGlob': return [];
+    case 'File': return [node.StmtList];
+    case 'ForClause': return [node.Do, node.Loop];
+    case 'FuncDecl': return [node.Body, node.Name];
+    case 'IfClause': return [
+      node.Cond,
+      node.Then,
+      ...node.Else ? [node.Else] : [],
+    ];
+    case 'LetClause': return node.Exprs;
+    case 'Lit': return [];
+    case 'ParamExp': return [
+      ...(node.Exp?.Word ? [node.Exp.Word] : []),
+      ...(node.Index ? [node.Index] : []),
+      node.Param,
+      ...(node.Repl ? [node.Repl.Orig] : []),
+      ...(node.Repl?.With ? [node.Repl.With] : []),
+      ...(node.Slice ? [node.Slice.Offset] : []),
+      ...(node.Slice?.Length ? [node.Slice.Length] : [])
+    ];
+    case 'ParenArithm': return [node.X];
+    case 'ParenTest': return [node.X];
+    case 'ProcSubst': return [node.StmtList];
+    case 'Redirect': return [
+      ...(node.Hdoc ? [node.Hdoc] : []),
+      ...(node.N ? [node.N] : []),
+      node.Word,
+    ];
+    case 'SglQuoted': return [];
+    case 'Stmt': return [
+      ...node.Cmd ? [node.Cmd] : [],
+      ...node.Redirs,
+    ];
+    case 'StmtList': return node.Stmts;
+    case 'Subshell': return [node.StmtList];
+    case 'TestClause': return [node.X];
+    case 'TimeClause': return [
+      ...node.Stmt ? [node.Stmt] : [],
+    ];
+    case 'UnaryArithm':
+    case 'UnaryTest': return [node.X];
+    case 'WhileClause': return [node.Cond, node.Do];
+    case 'Word': return node.Parts;
+    case 'WordIter': return node.Items;
+    default: throw testNever(node);
+  }
+}
+
+export function traverse(node: ParsedSh, act: (node: ParsedSh) => void) {
+  act(node);
+  getChildren(node).forEach(child => traverse(child, act));
+}
+
+export function withParents<T extends ParsedSh>(root: T) {
+  traverse(root, (node) => {
+    getChildren(node).forEach(child => (child as BaseNode).parent = node);
+  });
+  return root;
+}
