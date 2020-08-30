308278ad08be0c4aee645431a7a0ad3dda1b5712
Robert S.R. Myers <me.robmyers@gmail.com>
2020-08-30 17:21:20 +0100

    need lookup utils since immer breaking due to recursion

---
+++ store/store.util.ts
@@ -0,0 +1,43 @@
+import { KeyedLookup } from "@model/generic.model";
+
+export function addToLookup<LookupItem extends { key: string }>(
+  newItem: LookupItem,
+  lookup: KeyedLookup<LookupItem>
+): KeyedLookup<LookupItem> {
+  return { ...lookup, [newItem.key]: newItem };
+}
+
+export function removeFromLookup<LookupItem extends { key: string }>(
+  itemKey: string,
+  lookup: KeyedLookup<LookupItem>
+): KeyedLookup<LookupItem> {
+  const { [itemKey]: _, ...rest } = lookup;
+  return rest;
+}
+
+export function updateLookup<LookupItem extends { key: string }>(
+  itemKey: string,
+  lookup: KeyedLookup<LookupItem>,
+  updater: ReduxUpdater<LookupItem>
+): KeyedLookup<LookupItem> {
+
+  if (!lookup[itemKey]) {
+    return lookup;
+  }
+  const updates = updater(lookup[itemKey]);
+  if (!updates) { // falsy means don't update
+    return lookup; 
+  }
+
+  return {
+    ...lookup,
+    [itemKey]: {
+      ...lookup[itemKey],
+      ...updates
+    }
+  };
+}
+
+export type ReduxUpdater<LookupItem extends { key: string }> = (
+  item: LookupItem
+) => Partial<LookupItem>;
