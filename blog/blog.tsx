3664dc9cfbb7b21ce66a92923f9bcfae019fb2ca
Robert S.R. Myers <me.robmyers@gmail.com>
2020-09-05 16:50:11 +0100

    prefer scrollIntoView for hash nav, lets apply to all navs

--- blog/blog.tsx
+++ blog/blog.tsx
@@ -4,12 +4,13 @@ import Blog1 from './1/blog-1.mdx';
 import Blog2 from './2/blog-2.mdx';
 import css from './blog.scss';
 
+// TODO use scrollIntoView for all hash views
 const BlogRoot: React.FC = () => {
   useEffect(() => {
     if (getWindow()?.location.hash) {// Scroll to initial hash
       const found = Array.from(document.getElementsByTagName('a'))
         .find(el => el.name === window.location.hash.slice(1));
-      found && setTimeout(() => window.scrollTo({ top: found.offsetTop }), 100);
+      found && setTimeout(() => found.scrollIntoView({ behavior: 'smooth', block: 'start' }), 100);
     }
   }, []);
 
