1d7019bd78ac8aee708bd63d37c3e2f7247fd02a
Robert S.R. Myers <me.robmyers@gmail.com>
2020-09-11 20:15:13 +0100

    remove reset of filename expansion

--- model/shell/file.service.ts
+++ model/shell/file.service.ts
@@ -1,4 +1,3 @@
-import globRex from 'globrex';
 import { firstAvailableInteger } from '@model/generic.model';
 import useStore, { Process } from '@store/shell.store';
 import { varService } from './var.service';
@@ -18,56 +17,6 @@ export class FileService {
     writable: ShellStream<W>,
   ): FsFile {
     return new FsFile(absPath, new ShellFile(readable, writable));
-  }
-
-  /**
-   * Fundamental expansion of filepaths containing glob symbols
-   * \* (star), ? (question mark) or [] (square brackets).
-   */
-  expandFilepath(dirPath: string, glob: string) {
-    const dirParts = dirPath.split('/');
-    dirPath.endsWith('/') && dirParts.pop();
-    return this.expandFilepathParts(dirParts, glob.split('/'));
-  }
-
-  private expandFilepathParts(dirParts: string[], parts: string[]): string[] {
-    if (!parts.length) {
-      return [];
-    }
-
-    const first = parts.shift() as string;
-    if (first === '.') {
-      return this.expandFilepathParts(dirParts, parts);
-    } else if (first === '..') {
-      dirParts.pop();
-      return this.expandFilepathParts(dirParts, parts);
-    } else if (first === '') {// We're at the root of the filesystem
-      return this.expandFilepathParts([], parts).map(x => `/${x}`);
-    }
-
-    const currentDir = `/${dirParts.join('/')}/`
-    // Start with filenames of directories in 'current directory'
-    let matches = Object.keys(this.getFs()).map(absPath => {
-        if (absPath.startsWith(currentDir)) {
-          const rest = absPath.slice(currentDir.length).split('/');
-          return rest.length > 1 ? rest[0] : undefined;
-        }
-      }).filter(Boolean) as string[];
-
-    try {// npm module 'globrex' converts glob expression to js RegExp
-      const { regex } = globRex(first , { extended: true });
-      matches = matches.filter((x) => regex.test(x));
-    } catch (e) {// Error thrown e.g. if `first` is [ or [ ]
-      console.error(e);
-    }
-
-    if (!parts.length) {// Base case
-      return matches;
-    }
-
-    return matches.flatMap((dirName) =>
-      this.expandFilepathParts(dirParts.concat(dirName), parts)
-        .map((x) => `${dirName}/${x}`));
   }
 
   getFile(absPath: string): FsFile | null {
