4ece6b8f64d17314972be7ca476cf3625ecf4f83
Robert S.R. Myers <me.robmyers@gmail.com>
2020-09-22 06:55:22 +0100

    fix three-pathfinding types

--- types/three-pathfinding/index.d.ts
+++ types/three-pathfinding/index.d.ts
@@ -6,27 +6,27 @@ declare module 'three-pathfinding' {
     setZoneData(zoneId: string, zone: Zone): void;
     getRandomNode(zoneID: string, groupID: number, nearPosition: THREE.Vector3, nearRange: number): Node;
     getClosestNode(position: THREE.Vector3, zoneID: string, groupID: number, checkPolygon = false): Node;
-    findPath(startPosition: THREE.Vector3, endPosition: THREE.Vector3, zoneID: string, groupID: number): Vector3[];
+    findPath(startPosition: THREE.Vector3, endPosition: THREE.Vector3, zoneID: string, groupID: number): THREE.Vector3[];
     getGroup(zoneID: string, position: THREE.Vector3): number;
     clampStep(start: THREE.Vector3, end: THREE.Vector3, node: Node, zoneID: string, groupID: number, endTarget: THREE.Vector3): Node;
-    static createZone(): THREE.BufferGeometry;
+    static createZone(geometry: THREE.BufferGeometry): Zone;
 
   };
 
   class PathfindingHelper extends THREE.Object3D {
 
     setPath(path: Vector3[]): this;
-    setPlayerPosition(position: Vector3): this;
-    setTargetPosition(position: Vector3): this;
-    setNodePosition(position: Vector3): this;
-    setStepPosition(position: Vector3): this;
+    setPlayerPosition(position: THREE.Vector3): this;
+    setTargetPosition(position: THREE.Vector3): this;
+    setNodePosition(position: THREE.Vector3): this;
+    setStepPosition(position: THREE.Vector3): this;
     reset(): this;
 
   }
 
   interface Zone {
     groups: Group[];
-    vertices: Vector3[];
+    vertices: THREE.Vector3[];
   }
 
   type Group = Node[];
