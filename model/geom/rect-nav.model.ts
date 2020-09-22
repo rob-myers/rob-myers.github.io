71a649b1aaeed81d1d692a5d371697f373de81dd
Robert S.R. Myers <me.robmyers@gmail.com>
2020-09-22 09:01:01 +0100

    simplify RectNav

--- model/geom/rect-nav.model.ts
+++ model/geom/rect-nav.model.ts
@@ -1,6 +1,4 @@
-import { Polygon, PolygonJson } from "./polygon.model";
 import { Rect, RectJson } from "./rect.model";
-import { Vector } from "./vector.model";
 
 export const navInset = 10;
 export const navBoundsOutset = 50;
@@ -16,14 +14,8 @@ export class RectNavGraph {
     w: Rect[];
   }>;
 
-  /** Rectangle as polygon with adjacent rects corner points */
-  public toPolygon: Map<Rect, Polygon>;
-
-  constructor(
-    public rects: Rect[],
-  ) {
+  constructor(public rects: Rect[]) {
     this.succ = new Map;
-    this.toPolygon = new Map;
   }
 
   public compute() {
@@ -40,29 +32,12 @@ export class RectNavGraph {
         s: all.filter(o => r.y + r.height === o.y).sort(sort('x', 1)),
         w: all.filter(o => r.x === o.x + o.width).sort(sort('y', -1)),
       });
-
-      const { n, s, e, w } = this.succ.get(r)!;
-      /**
-       * Polyanya views +y as upwards and expects relatively anticlockwise polygons.
-       * We agree with polyanya's convention, which is more conventional in mathematics.
-       * North-east-south-west traversal wrt y+ down is: clockwise wrt y+ down, anticlockwise wrt y+ up.
-       */
-      this.toPolygon.set(r, new Polygon([
-        // along north-side (left to right)
-        r.nw, ...n.flatMap(o => [o.x > r.x && o.sw, o.e < r.e && o.se]).filter(Boolean) as Vector[],
-        // along east-side (top to bottom)
-        r.ne, ...e.flatMap(o => [o.y > r.y && o.nw, o.s < r.s && o.sw]).filter(Boolean) as Vector[],
-        // along south-side (right to left)
-        r.se, ...s.flatMap(o => [o.e < r.e && o.ne, o.x > r.x && o.nw]).filter(Boolean) as Vector[],
-        // along west-side (bottom to top)
-        r.sw, ...w.flatMap(o => [o.s < r.s && o.se, o.y > r.y && o.ne]).filter(Boolean) as Vector[],
-      ]));
     });
 
     return this;
   }
 
-  public static from({ rects, succ, toPolygon }: RectNavGraphJson) {
+  public static from({ rects, succ }: RectNavGraphJson) {
     const output = new RectNavGraph(rects.map(x => Rect.from(x)));
     succ.forEach(([key, value]) => output.succ.set(Rect.from(key), {
       all: value.all.map(x => Rect.from(x)),
@@ -71,8 +46,6 @@ export class RectNavGraph {
       s: value.s.map(x => Rect.from(x)),
       w: value.w.map(x => Rect.from(x)),
     }));
-    toPolygon.forEach(([key, value]) =>
-      output.toPolygon.set(Rect.from(key), Polygon.from(value)));
     return output;
   }
 
@@ -89,8 +62,6 @@ export class RectNavGraph {
             w: w.map(x => x.json),
           }
         ]),
-      toPolygon: Array.from(this.toPolygon)
-        .map(([src, dst]) => [src.json, dst.json])
     };
   }
 
@@ -105,7 +76,6 @@ export interface RectNavGraphJson {
     s: RectJson[];
     w: RectJson[];
   }][],
-  toPolygon: [RectJson, PolygonJson][],
 }
 
 export interface NavInput {
