7d961f64eaba34242689754bd41242d6e1563d30
Robert S.R. Myers <me.robmyers@gmail.com>
2021-02-27 20:06:18 +0000

    _app now typescript

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+import { NextComponentType, NextPageContext } from 'next';
+import { AppInitialProps } from 'next/app';
+import { Router } from 'next/router';
+import '../styles/globals.css'
+
+const MyApp: React.FC<RootProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+interface RootProps extends AppInitialProps {
+  Component: NextComponentType<NextPageContext, any, {}>;
+  router: Router;
+}
+
+export default MyApp
