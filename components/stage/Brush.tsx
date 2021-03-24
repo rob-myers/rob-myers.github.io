3eba9b28dc6da72362b5f607918d982b8b43e102
Robert S.R. Myers <me.robmyers@gmail.com>
2021-03-24 18:13:08 +0000

    smoother brush resize; decided against brush drag

--- components/stage/Brush.tsx
+++ components/stage/Brush.tsx
@@ -5,8 +5,8 @@ import { DoubleSide, Vector3 } from "three";
 import { VectorJson } from "model/geom";
 import { ndCoordsToGroundPlane, vectAccuracy } from "model/3d/three.model";
 import { brushPolyName, brushRectName, computeBrushStyles, StoredStage } from "model/stage/stage.model";
-import useStageStore from "store/stage.store";
 import { geomService } from "model/geom.service";
+// import useStageStore from "store/stage.store";
 
 const Brush: React.FC<Props> = ({ wire, stage }) => {
   const root = useRef<THREE.Group>(null);
@@ -20,27 +20,27 @@ const Brush: React.FC<Props> = ({ wire, stage }) => {
   const [everUsed, setEverUsed] = useState(false);
   
   useEffect(() => {
+    const group = root.current!;
+
     const sub = wire.subscribe(({ key, ndCoords }) => {
       if (key === 'pointerleave' || key === 'pointerup') {
-        if (active.current && root.current) {// Sync the state
-          stage.brush.position.copy(root.current.position);
-          stage.brush.scale.copy(root.current.scale);
-        }
         active.current = false;
-      } else if (key === 'pointerdown') {
-        const groundPoint = new Vector3;
-        ndCoordsToGroundPlane(groundPoint, ndCoords, camera);
-        if (useStageStore.api.pointInBrush(stage.key, groundPoint)) {
-          return; // Don't reselect if start inside selection
-        }
+        // Sync with state
+        stage.brush.position.copy(group.position);
+        stage.brush.scale.copy(vectAccuracy(group.scale, 1));
 
+      } else if (key === 'pointerdown') {
+        ndCoordsToGroundPlane(initial, ndCoords, camera);
+        // if (useStageStore.api.pointInBrush(stage.key, initial)) {
+        //   return; // Don't reselect if start inside selection
+        // }
         active.current = true;
-        vectAccuracy(initial.copy(groundPoint), 1);
-        root.current?.position.set(initial.x, initial.y, 0);
+        vectAccuracy(initial, 1);
+        group.position.set(initial.x, initial.y, 0);
+        group.scale.set(0, 0, 0);
       } else if (key === 'pointermove' && active.current) {
         ndCoordsToGroundPlane(current, ndCoords, camera);
-        vectAccuracy(current, 1);
-        root.current?.scale.set(current.x - initial.x, -(current.y - initial.y), 1);
+        group.scale.set(current.x - initial.x, -(current.y - initial.y), 1);
         !everUsed && setEverUsed(true);
       }
     });
