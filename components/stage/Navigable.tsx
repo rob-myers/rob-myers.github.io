57c0041c57eae802ee44ccecc0b6db362721b194
Robert S.R. Myers <me.robmyers@gmail.com>
2021-04-11 07:03:14 +0100

    single memo for Navigable

--- components/stage/Navigable.tsx
+++ components/stage/Navigable.tsx
@@ -12,21 +12,17 @@ const Navigable: React.FC<Props> = ({
   const { polygons: wallsPolys } = polygon[CorePolygonKey.walls];
   const { polygons: navPolys } = polygon[CorePolygonKey.navigable];
   
-  /** Lighting and shadows drawn outside wall bases */
-  const notWallsGeom = useMemo(() => {
-    const inverted = geomService.invert(wallsPolys, bounds);
-    return geomService.polysToGeometry(inverted);
-  }, [bounds, wallsPolys]);
+  const { notWallsGeom, unNavigableGeom, wallsGeom } = useMemo(() => {
+    return {
+      /** Lighting and shadows drawn outside wall bases */
+      notWallsGeom: geomService.polysToGeometry(
+        geomService.invert(wallsPolys, bounds)),
+      unNavigableGeom: geomService.polysToGeometry(
+        geomService.invert(navPolys, bounds)),
+      wallsGeom: geomService.polysToGeometry(wallsPolys),
+    }
+  }, [bounds, navPolys, wallsPolys]);
   
-  const unNavigableGeom = useMemo(() => {
-    const inverted = geomService.invert(navPolys, bounds);
-    return geomService.polysToGeometry(inverted);
-  }, [bounds, navPolys]);
-
-  const wallsGeom = useMemo(() =>
-    geomService.polysToGeometry(wallsPolys, 'xy', 0),
-  [wallsPolys]);
-
   return (
     <group>
       <mesh
