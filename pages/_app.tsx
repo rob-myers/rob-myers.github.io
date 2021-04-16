223ad4ee2f8a7d3a3abccf61e2759d325b7fdbf8
Robert S.R. Myers <me.robmyers@gmail.com>
2021-04-16 12:43:09 +0100

    loadTextures on mount app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
+import { useEffect } from 'react';
 import { NextComponentType, NextPageContext } from 'next';
 import { AppInitialProps } from 'next/app';
 import { Router } from 'next/router';
+import useGeomStore from 'store/geom.store';
 
 import 'xterm/css/xterm.css';
 import 'styles/globals.css'
 
 const PagesRoot: React.FC<RootProps> = ({ Component, pageProps }) => {
+  useEffect(() => { useGeomStore.api.loadTextures() }, []);
   return <Component {...pageProps} />
 }
 
