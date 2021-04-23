35e7f4532c563a00ce1f92caa5390e8606895bcb
Robert S.R. Myers <me.robmyers@gmail.com>
2021-04-23 10:57:23 +0100

    fix types

--- components/stage/CameraControls.tsx
+++ components/stage/CameraControls.tsx
@@ -3,7 +3,7 @@ import { extend, useThree, useFrame } from '@react-three/fiber';
 import { PanZoomControls } from 'model/3d/pan-zoom-controls';
 import { StageInternal } from 'model/stage/stage.model';
 
-// See types/react-three-fiber/three-types.d.ts
+// See types/three-types.d.ts
 extend({ PanZoomControls });
 
 const CameraControls: React.FC<Props> = ({ internal, enabled }) => {
