01db4549601b810c66f7ba9b500d863041440ff5
Robert S.R. Myers <me.robmyers@gmail.com>
2021-05-07 15:56:52 +0100

    fix three/examples/jsm facade using soft links

--- model/3d/facade.ts
+++ model/3d/facade.ts
@@ -1,21 +1,17 @@
 /**
  * We use this facade to prevent three.js from breaking server-side rendering.
+ * We could use dynamic import() but awaiting it can delay initial render.
+ *
+ * - Geometry.js was created via:
+ *   `ln -s ../../node_modules/three/examples/jsm/deprecated/Geometry.js model/3d/Geometry.js`
+ * - SkeletonUtils.js was created via:
+ *   `ln -s ../../node_modules/three/examples/jsm/utils/SkeletonUtils.js model/3d/SkeletonUtils.js`
  */
 import type { SkeletonUtils as SkeletonUtilsNamespace } from 'three/examples/jsm/utils/SkeletonUtils';
-export let SkeletonUtils: typeof SkeletonUtilsNamespace;
+import { SkeletonUtils as SkeletonUtilsValue } from './SkeletonUtils';
+export const SkeletonUtils = SkeletonUtilsValue as typeof SkeletonUtilsNamespace;
 
 import type { Geometry as GeometryClass, Face3 as Face3Class } from 'three/examples/jsm/deprecated/Geometry';
-export let Geometry: typeof GeometryClass;
-export let Face3: typeof Face3Class;
-
-import { getWindow } from 'model/dom.model';
-
-if (getWindow()) {
-  import('three/examples/jsm/utils/SkeletonUtils').then((x) => {
-    SkeletonUtils = x.SkeletonUtils;
-  });
-  import('three/examples/jsm/deprecated/Geometry').then((x) => {
-    Geometry = x.Geometry;
-    Face3 = x.Face3;
-  });
-}
+import { Geometry as GeometryValue, Face3 as Face3Value } from './Geometry';
+export const Geometry = GeometryValue as unknown as typeof GeometryClass;
+export const Face3 = Face3Value as typeof Face3Class;
