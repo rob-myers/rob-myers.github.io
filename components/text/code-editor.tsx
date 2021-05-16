a7dc8c79f91d632f87d8c48e371ad7e87782bab4
Robert S.R. Myers <me.robmyers@gmail.com>
2021-05-16 10:51:07 +0100

    clean

--- components/text/code-editor.tsx
+++ components/text/code-editor.tsx
@@ -1,19 +1,38 @@
-import PrismEditor from "./prism-editor"
+import React from "react";
 import styled from "@emotion/styled";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import "prismjs/themes/prism-tomorrow.css";
 
 const CodeEditor: React.FC = () => {
-  return (
-    <Root>
-      <PrismEditor />
-    </Root>
+  const [code, setCode] = React.useState(
+    `function add(a, b) {\n  return a + b;\n}`
   );
-};
+  return (
+    <Root
+      value={code}
+      onValueChange={(code) => setCode(code)}
+      highlight={(code) => highlight(code, languages.javascript, 'javascript')}
+      padding={10}
+      style={{
+        fontFamily: '"Fira code", "Fira Mono", monospace',
+        fontSize: 12,
+        // TODO remove hard-coding
+        minHeight: 400,
+        // prism-tomorrow specific:
+        background:'#2d2d2d',
+        color: '#ccc',
+      }}
+    />
+  );
+}
 
-export default CodeEditor;
-
-const Root = styled.section<{}>`
+const Root = styled<typeof Editor>(Editor)`
   grid-area: code;
   background: #000;
   overflow: scroll;
-  /* max-height: 100; */
 `;
+
+export default CodeEditor;
