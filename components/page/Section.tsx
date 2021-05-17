d0fb151d76d00791016c419d8fae3cf1c32d945f
Robert S.R. Myers <me.robmyers@gmail.com>
2021-05-17 13:45:05 +0100

    Section has larger padding

--- components/page/Section.tsx
+++ components/page/Section.tsx
@@ -3,6 +3,6 @@ import styled from "@emotion/styled";
 export const Section = styled.section<{}>`
   border-radius: 5px;
   border: 1px solid #ddd;
-  padding: 8px;
+  padding: 18px;
   margin-bottom: 24px;
 `;
