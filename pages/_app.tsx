cb7cc99ed885db83c275aa8c58c80b9705aac87d
Robert S.R. Myers <me.robmyers@gmail.com>
2021-05-21 16:17:05 +0100

    expose window.THREE for code.lib shortcut

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,14 @@ import { Router } from 'next/router';
 
 import 'xterm/css/xterm.css';
 import 'styles/globals.css'
+import { useEffect } from 'react';
+import * as THREE from 'three';
 
 const PagesRoot: React.FC<RootProps> = ({ Component, pageProps }) => {
+  useEffect(() => {
+    window.THREE = THREE; // Shortcut for code.lib
+  }, []);
+
   return <Component {...pageProps} />
 }
 
