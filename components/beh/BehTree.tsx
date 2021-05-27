e0287ffcdedf5d4ed4cb4a4090e8333c1c2ea7a9
Robert S.R. Myers <me.robmyers@gmail.com>
2021-05-27 18:29:54 +0100

    added react-diagrams demo

--- components/beh/BehTree.tsx
+++ components/beh/BehTree.tsx
@@ -1,11 +1,12 @@
-import { useRef } from "react";
+import dynamic from 'next/dynamic';
 import styled from "@emotion/styled";
 
-export default function BehTree() {
-  const containerRef = useRef<HTMLElement>(null);
+const ReactDiagram = dynamic(() => import('./ReactDiagram'), { ssr: false });
 
+export default function BehTree() {
   return (
-    <Root ref={containerRef}>
+    <Root>
+      <ReactDiagram />
     </Root>
   );
 }
@@ -14,4 +15,44 @@ const Root = styled.section<{}>`
   grid-area: beh;
   border: 1px solid #ccc;
   height: 100%;
+
+  .diagram-container{
+    background: #333333;
+    width: 100%;
+    height: 100%;
+  }
+
+  .custom-node{
+    border: solid 2px gray;
+    border-radius: 5px;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+    position: relative;
+  }
+
+  .custom-node-color{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 20px;
+    height: 20px;
+    transform: translate(-50%, -50%);
+    border-radius: 10px;
+  }
+
+  .circle-port{
+    width: 12px;
+    height: 12px;
+    margin: 2px;
+    border-radius: 4px;
+    background: darkgray;
+    cursor: pointer;
+  }
+
+  .circle-port:hover{
+    background: mediumpurple;
+  }
 `;
---
+++ components/beh/ReactDiagram.tsx
@@ -0,0 +1,101 @@
+import { useEffect, useState } from "react";
+import { AbstractReactFactory, GenerateWidgetEvent, CanvasEngineOptions, SelectionBoxLayerFactory, CanvasWidget } from '@projectstorm/react-canvas-core';
+import { DiagramModel, DiagramEngine, NodeModel, PortWidget, NodeLayerFactory, LinkLayerFactory, DefaultDiagramState } from '@projectstorm/react-diagrams-core';
+import { DefaultPortModel, DefaultLabelFactory, DefaultLinkFactory, DefaultNodeFactory, DefaultLinkModel, DefaultPortFactory } from '@projectstorm/react-diagrams-defaults';
+
+export default function ReactDiagram() {
+  const [engine, setEngine] = useState<DiagramEngine>();
+  
+  useEffect(() => {
+    const engine = createEngine();
+    engine.getNodeFactories().registerFactory(new CustomNodeFactory);
+    // console.log({ engine });
+    
+    const node1 = new CustomNodeModel({ color: 'rgb(192,255,0)' });
+    node1.setPosition(50, 50);
+    const node2 = new CustomNodeModel({ color: 'rgb(0,192,255)' });
+    node2.setPosition(200, 50);
+    const link1 = new DefaultLinkModel;
+    link1.setSourcePort(node1.getPort('out')!)
+    link1.setTargetPort(node1.getPort('in')!)
+
+    const model = new DiagramModel;
+    model.addAll(node1, node2, link1);
+    engine.setModel(model);
+    setEngine(engine);
+  }, []);
+
+  return engine
+    ? <CanvasWidget className="diagram-container" engine={engine} />
+    : null;
+}
+
+function createEngine(options: CanvasEngineOptions = {}): DiagramEngine {
+  const engine = new DiagramEngine(options);
+  // register model factories
+  engine.getLayerFactories().registerFactory(new NodeLayerFactory() as any);
+  engine.getLayerFactories().registerFactory(new LinkLayerFactory() as any);
+  engine.getLayerFactories().registerFactory(new SelectionBoxLayerFactory());
+
+  engine.getLabelFactories().registerFactory(new DefaultLabelFactory());
+  engine.getNodeFactories().registerFactory(new DefaultNodeFactory()); // i cant figure out why
+  engine.getLinkFactories().registerFactory(new DefaultLinkFactory());
+  // engine.getLinkFactories().registerFactory(new PathFindingLinkFactory());
+  engine.getPortFactories().registerFactory(new DefaultPortFactory());
+
+  // register the default interaction behaviours
+  engine.getStateMachine().pushState(new DefaultDiagramState());
+  return engine;
+};
+
+class CustomNodeFactory extends AbstractReactFactory<CustomNodeModel, DiagramEngine> {
+  constructor() {
+    super('custom-node');
+  }
+
+  generateModel() {
+    return new CustomNodeModel({});
+  }
+
+  generateReactWidget(event: GenerateWidgetEvent<CustomNodeModel>) {
+    return <CustomNodeWidget engine={this.engine} node={event.model} />;
+  }
+}
+
+class CustomNodeModel extends NodeModel {
+  color: string;
+
+  constructor(opts: { color?: string }) {
+    super({ ...opts, type: 'custom-node' });
+    this.color = opts.color || 'red';
+
+    this.addPort(new DefaultPortModel({ in: true, name: 'in' }));
+    this.addPort(new DefaultPortModel({ in: false, name: 'out' }));
+  }
+
+  serialize() {
+    return { ...super.serialize(), color: this.color };
+  }
+
+  deserialize(event: any): void {
+    super.deserialize(event);
+    this.color = event.data.color;
+  }
+}
+
+function CustomNodeWidget(props: {
+  engine: DiagramEngine;
+  node: CustomNodeModel;
+}) {
+  return (
+    <div className="custom-node">
+      <PortWidget engine={props.engine} port={props.node.getPort('in')!}>
+        <div className="circle-port" />
+      </PortWidget>
+      <PortWidget engine={props.engine} port={props.node.getPort('out')!}>
+        <div className="circle-port" />
+      </PortWidget>
+      <div className="custom-node-color" style={{ backgroundColor: props.node.color }} />
+    </div>
+  );
+}
