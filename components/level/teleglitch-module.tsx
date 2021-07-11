74c5f07078c1c824aa54134b8a99389670ebe6b7
Robert S.R. Myers <me.robmyers@gmail.com>
2021-07-11 14:54:06 +0100

    can render module items of type 'bmp'

--- components/level/teleglitch-module.tsx
+++ components/level/teleglitch-module.tsx
@@ -14,14 +14,14 @@ export default function TeleglitchModule() {
   const moduleCanvas = useRef<HTMLCanvasElement>(null);
 
   const { data: teleglitch } = useQuery<Teleglitch>('teleglitch', async () => {
-    const spritesheet = new Image;
-    spritesheet.src = '/api/teleglitch/gfx?set1.png';
+    const img = new Image;
+    img.src = '/api/teleglitch/gfx?set1.png';
   
     const [canvas, gfx, modules, objects] = await Promise.all([
-      spritesheet.decode().then(() => {
+      img.decode().then(() => {
         const canvas = document.createElement('canvas');
-        [canvas.width, canvas.height] = [spritesheet.width, spritesheet.height];
-        canvas.getContext('2d')!.drawImage(spritesheet, 0, 0);
+        [canvas.width, canvas.height] = [img.width, img.height];
+        canvas.getContext('2d')!.drawImage(img, 0, 0);
         return canvas;
       }),
       fetch('/api/teleglitch/lua?gfx.json').then(handleJsonFetch),
@@ -29,16 +29,37 @@ export default function TeleglitchModule() {
       fetch('/api/teleglitch/lua?objects.json').then(handleJsonFetch),
     ]);
 
-    return { canvas, gfx, modules, objects };
+    return { spritesheet: canvas, gfx, modules, objects };
   }, useQueryOptions);
 
   useEffect(() => {
     if (teleglitch) {// TODO draw a module
-      const module = teleglitch.modules.find(x => x.moduleName === 'l1_2')!;
-      console.log(module);
+      const ctxt = moduleCanvas.current!.getContext('2d')!;
+      const sheet = teleglitch.spritesheet;
+      // const module = teleglitch.modules.find(x => x.moduleName === 'algus3')!;
+      // const module = teleglitch.modules.find(x => x.moduleName === 'l1_konservi_ladu')!;
+      const module = teleglitch.modules.find(x => x.moduleName === 'l1_v2ike_ringaed')!;
+      console.log(module, sheet);
+
+      ctxt.resetTransform();
+      ctxt.clearRect(0, 0, ctxt.canvas.width, ctxt.canvas.height);
+      ctxt.translate(100, 40);
+      ctxt.scale(10, 10);
+      ctxt.scale(2, 1); // strange
+      ctxt.strokeStyle = 'blue';
+      ctxt.lineWidth = 0.05;
+      const scale = 12; // important
+
       for (const item of module.items) {
         if (item.type === 'bmp' && item.tex === 'gfx/set1.png') {
-          // TODO
+          const [w, h] = [item.x2 - item.x1, item.y2 - item.y1];
+          const [dw, dh] = [w / scale, h / scale];
+          ctxt.save();
+          ctxt.translate(item.x + (dw/2), item.y + (dh/2));
+          ctxt.rotate(item.angle);
+          ctxt.drawImage(sheet, item.x1, item.y1, w, h, -dw/2, -dh/2, dw, dh);
+          // ctxt.strokeRect(-dw/2, -dh/2, dw, dh);
+          ctxt.restore();
         } else if (item.type in teleglitch.objects) {
           // TODO
         } else if (item.type === 'node') {
@@ -58,13 +79,20 @@ export default function TeleglitchModule() {
   }, [teleglitch]);
 
   return (
-    <canvas ref={moduleCanvas} style={{ border: '1px solid red' }} />
+    <canvas
+      ref={moduleCanvas}
+      style={{
+        border: '1px solid #555',
+        width: 400,
+        height: 400,
+      }}
+    />
   );
 }
 
 interface Teleglitch {
   /** Spritesheet `set1.png` */
-  canvas: HTMLCanvasElement;
+  spritesheet: HTMLCanvasElement;
   gfx: Teleglitch.Gfx;
   modules: Teleglitch.Mods;
   objects: Teleglitch.Objects;
