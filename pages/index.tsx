71fd29cdbd8d38c25b615bae55380e4795d4b3cf
Robert S.R. Myers <me.robmyers@gmail.com>
2021-07-20 13:15:31 +0100

    work on intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,23 +23,34 @@ export default function IndexPage() {
 # react re-spec
 
 towards integrated unit-testing
-`}/>
+        `}/>
 
         <Markdown children={`
 ## Statement of intent <float rem="1.2">19th July 2021</float>
 
-Web applications are built from _components_ satisfying assertions called _unit tests_. Frontend developers alternate between updating components and updating the respective assertions (amongst other jobs). Sometimes they fix stale tests; sometimes they extend a component to satisfy a test. Unit tests also provide documentation, by enumerating expected use cases.
+Web applications are built from _components_ satisfying assertions called _unit tests_. Frontend developers alternate between updating components and updating the respective assertions (amongst other jobs). Sometimes they fix stale tests; sometimes they extend a component to satisfy a test. Unit tests also provide important documentation by explicitly describing expected functionality.
 
 The objective of this website is:
-> _to make unit-testing more automatic, interactive and fun_.
+> to reintegrate unit-tests into the frontend development process, making them automatic, interactive and fun.
 
-Concerning automation, we'll apply static analysis to obtain a partially-specified state machine from each component. Concerning interactivity, we want to [play-in](https://link.springer.com/book/10.1007/978-3-642-19029-2) assertions i.e. record input and desired states.
+#### How we'll do it
+
+Using static analysis we'll construct a _model_ of each component i.e. some kind of partially specified state-machine. By interacting with the component we can [play-in](https://link.springer.com/book/10.1007/978-3-642-19029-2) assertions (record input/ouput), thereby enriching the model. Finally we'll use the model to generate unit-tests, coverage reports, and answer queries.
+
+Eventually we envisage a system not unlike [Storybook](https://storybook.js.org/), where components, models and their induced unit tests can be viewed.
+
+#### How it'll be fun
+
+Instead of writing additional code we're going to create unit-tests via interaction, which is more meaningful and more enjoyable. But for the process to be fun, the components should be fun to work with. We'll provide a large number of examples:
+- from projects freely available on GitHub.
+- from a topdown game we'll build step-by-step.
 
 
 _TODO_ ...
 
         `}/>
 
+        <section style={{ background: '#000', height: 94 }}>
           <CodeEditor
             height="auto"
             padding="16px 24px"
@@ -49,6 +60,7 @@ function f(x, y) { return (x - 1) / (y - 1); }
 // or alternatively:
 const f = (x, y) => (x - 1) / (y - 1);
           `} />
+        </section>
 
         <Markdown children={`
 _TODO_ ...
@@ -62,11 +74,13 @@ _TODO hookup babel transpilation of jsx using forked @babel/standalone_
 
         <br/>
 
-        <CodeEditor
-          height="400px"
-          code={initialCode['file.js']}
-          lineNumbers
-        />
+        <section style={{ background: '#000', height: 400 }}>
+          <CodeEditor
+            height="400px"
+            code={initialCode['file.js']}
+            lineNumbers
+          />
+        </section>
 
         <Markdown children={`
 This is an example of a [command link](#command "@test echo foo").
