a209dcaba31b3d7b6304cea1fc502a75fb81e3e5
Robert S.R. Myers <me.robmyers@gmail.com>
2021-07-20 10:49:41 +0100

    work on intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -22,18 +22,18 @@ export default function IndexPage() {
         <Markdown title children={`
 # react re-spec
 
-towards easier component design and testing
+towards integrated unit-testing
 `}/>
 
         <Markdown children={`
 ## Statement of intent <float rem="1.2">19th July 2021</float>
 
-Web applications are built from _components_ with behavioural assertions called _unit-tests_. They must pass before new code can be submitted. Frontend developers alternate between updating components and updating tests. Sometimes they _fix stale tests_; sometimes they _extend the component_ to satisfy a test. The tests also provide important documentation by specifying the expected use cases.
+Web applications are built from _components_ satisfying assertions called _unit tests_. Frontend developers alternate between updating components and updating the respective assertions (amongst other jobs). Sometimes they fix stale tests; sometimes they extend a component to satisfy a test. Unit tests also provide documentation, by enumerating expected use cases.
 
 The objective of this website is:
-> to make unit-testing more automatic and interactive.
+> _to make unit-testing more automatic, interactive and fun_.
 
-It can be made _more automatic_ via static analysis e.g. each component induces a partially-specified state machine. It can be made _more interactive_ by [playing-in](https://link.springer.com/book/10.1007/978-3-642-19029-2) specifications.
+Concerning automation, we'll apply static analysis to obtain a partially-specified state machine from each component. Concerning interactivity, we want to [play-in](https://link.springer.com/book/10.1007/978-3-642-19029-2) assertions i.e. record input and desired states.
 
 
 _TODO_ ...
