845069607dc3ab39a0bdadd8ee895bb89e4c74ec
Robert S.R. Myers <me.robmyers@gmail.com>
2021-07-31 09:41:10 +0100

    start using eslint for runtime jsx

--- components/runtime/PanZoom.jsx
+++ components/runtime/PanZoom.jsx
@@ -6,9 +6,10 @@ export default function PanZoom() {
       css={css`
         width: 100%;
         height: 100%;
-        background: #ff0;
+        background: #fff;
         /* position: absolute; */
       `}
+      // onWheel={onWheel}
     >
       <rect x={5} y={5} width={20} height={20} fill="red" />
     </svg>
