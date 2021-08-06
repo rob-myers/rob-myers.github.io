51aebb71bcb8dc05c3c2fa41cec4bd98fd0751fd
Robert S.R. Myers <me.robmyers@gmail.com>
2021-08-06 17:39:00 +0100

    better demo for panzoom

--- projects/panzoom/Demo.jsx
+++ projects/panzoom/Demo.jsx
@@ -3,7 +3,7 @@ import PanZoom from './PanZoom';
 
 export default function Demo() {
   return (
-    <>
+    <Container>
       <Title>
         Drag to pan, Scroll or Pinch to Zoom
       </Title>
@@ -12,14 +12,22 @@ export default function Demo() {
           <rect fill="red" x={10} y={10} width={20} height={20} />
         </PanZoom>
       </Outline>
-    </>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
 
 const Title = styled.div`
   padding: 8px;
 `;
 
 const Outline = styled.section`
+  flex: 1;
   border: 1px solid #aaa;
-`
+  position: relative;
+`;
