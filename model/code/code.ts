258411ed35dd87a28e480cf004341f7542b38e1c
Robert S.R. Myers <me.robmyers@gmail.com>
2021-08-08 12:45:03 +0100

    fix stale module specifier

--- model/code/code.ts
+++ model/code/code.ts
@@ -1,5 +1,5 @@
 //@ts-expect-error
-import panZoomJsx from '!!raw-loader!projects/panzoom/PanZoom.jsx';
+import panZoomJsx from '!!raw-loader!projects/components/PanZoom.jsx';
 
 export const initialCode = {
   'PanZoom.jsx': panZoomJsx,
