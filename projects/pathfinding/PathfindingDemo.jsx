457286d9bb8dedfac8eb9ccaf7b331e9688905da
Robert S.R. Myers <me.robmyers@gmail.com>
2021-08-25 23:10:17 +0100

    PathfindingDemo shows hollow octogon

--- projects/pathfinding/PathfindingDemo.jsx
+++ projects/pathfinding/PathfindingDemo.jsx
@@ -1,6 +1,29 @@
+import { Poly, Rect, Vect } from "../geom";
+import PanZoom from "../panzoom/PanZoom";
+
 export default function PathfindingDemo() {
 
-  console.info('PathfindingDemo');
-
-  return null;
+  return (
+    <section style={{ height: 300 }}>
+      <PanZoom gridBounds={gridBounds} initViewBox={initViewBox}>
+        <g transform="translate(150, 90)">
+          {/* <polygon points={String(octogon.outline)} /> */}
+          <path d={`M${hollowOctogon.outline}Z M${hollowOctogon.holes[0]}Z`} />
+        </g>
+      </PanZoom>
+    </section>
+  );
 }
+
+const gridBounds = new Rect(-5000, -5000, 10000 + 1, 10000 + 1);
+const initViewBox = new Rect(0, 0, 200, 200);
+
+const range = [...Array(8)].map((_ ,i) => i);
+const octogon = new Poly(range.map((_ ,i) => new Vect(
+  Math.cos(2 * Math.PI * (1/16 + i/8)),
+  Math.sin(2 * Math.PI * (1/16 + i/8)),
+))).scale(50);
+const [hollowOctogon] = Poly.cutOut(
+  [octogon.clone().scale(0.8)],
+  [octogon],
+);
