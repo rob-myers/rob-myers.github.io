3d541d8a4dc1efd0431b9ff8f16a0e26454f7ca0
Robert S.R. Myers <me.robmyers@gmail.com>
2021-09-01 10:25:01 +0100

    clean

--- projects/geomorph/UseSvg.jsx
+++ projects/geomorph/UseSvg.jsx
@@ -6,30 +6,29 @@ import { Poly, Rect } from '../geom';
 /** @param {{ url: string, transform?: string }} props */
 export default function UseSvg(props) {
   const { data } = useSvgText(props.url, props.transform);
-  return (
+
+  return data ? (
     <g className={`used ${props.url.slice('/svg/'.length)}`}>
       <g
         className="loaded"
         transform={props.transform}
         dangerouslySetInnerHTML={{
-          __html: data?.svgInnerText || '',
+          __html: data.svgInnerText || '',
         }}
       />
-      {data && (
-        <g className="meta">
-          {data.hull?.outline && (
-            <polygon fill="rgba(100, 0, 0, 0.2)" points={`${data.hull.outline}`} />
-          )}
-          {data.doors.map(({ outline }, i) =>
-            <polygon fill="rgba(0, 0, 200, 0.3)" key={i} points={`${outline}`} />
-          )}
-          {data.walls.map((poly, i) =>
-            <path fill="rgba(0, 200, 0, 0.5)" key={i} d={`${poly.svgPath}`} />
-          )}
-        </g>
-      )}
+      <g className="meta">
+        {data.hull?.outline && (
+          <polygon fill="rgba(100, 0, 0, 0.2)" points={`${data.hull.outline}`} />
+        )}
+        {data.doors.map(({ outline }, i) =>
+          <polygon fill="rgba(0, 0, 200, 0.3)" key={i} points={`${outline}`} />
+        )}
+        {data.walls.map((poly, i) =>
+          <path fill="rgba(0, 200, 0, 0.5)" key={i} d={`${poly.svgPath}`} />
+        )}
+      </g>
     </g>
-  );
+  ) : null;
 }
 
 /**
