eb8077959a77be01af26b484ebed1e8351d8b26d
Robert S.R. Myers <me.robmyers@gmail.com>
2021-09-10 15:56:49 +0100

    add `yarn watch-svg-meta`; only create svg.json

--- media/scripts/svg-meta.ts
+++ media/scripts/svg-meta.ts
@@ -1,8 +1,7 @@
 /**
  * Usage: yarn svg-meta
- * > Parse svg symbols in /public/symbol,
- * > writing json files in same directory,
- * > and a single json file /public/symbol/svg.json
+ * > Parse svg symbols in /public/symbol, writing
+ * > a single json file /public/symbol/svg.json
  */
 import fs from 'fs';
 import stringify from 'json-stringify-pretty-compact';
@@ -14,7 +13,8 @@ import {
   serializeSymbol,
 } from '../../projects/geomorph/parse-symbol';
 
-const symbolsDir = path.resolve(__dirname, '../../public/symbol');
+const publicDir = path.resolve(__dirname, '../../public');
+const symbolsDir = path.resolve(publicDir, 'symbol');
 const svgFilenames = fs.readdirSync(symbolsDir).filter(x => x.endsWith('.svg'));
 const svgJsonLookup = {} as Record<string, ParsedSymbol<GeoJsonPolygon>>;
 
@@ -23,8 +23,8 @@ for (const filename of svgFilenames) {
   const filepath = path.resolve(symbolsDir, filename);
   const contents = fs.readFileSync(filepath).toString();
   const parsed = serializeSymbol(parseStarshipSymbol(symbolName, contents));
-  const metaFilepath = path.resolve(symbolsDir, path.basename(filename) + '.json');
-  fs.writeFileSync(metaFilepath, stringify(parsed));
+  // const metaFilepath = path.resolve(symbolsDir, path.basename(filename) + '.json');
+  // fs.writeFileSync(metaFilepath, stringify(parsed));
   svgJsonLookup[symbolName] = parsed;
 }
 
