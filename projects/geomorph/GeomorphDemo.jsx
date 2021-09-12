3751084a4a5eca02c87f5848437481c0b46e248d
Robert S.R. Myers <me.robmyers@gmail.com>
2021-09-12 22:01:23 +0100

    extend 301 with individual consoles

--- projects/geomorph/GeomorphDemo.jsx
+++ projects/geomorph/GeomorphDemo.jsx
@@ -85,6 +85,8 @@ const layout301 = {
     { symbol: 'iris-valves--005--1x1', transform: [0, 1, 1, 0, 1140, 240] },
     { symbol: 'iris-valves--005--1x1', transform: [-1, 0, 0, 1, 360, 540] },
     { symbol: 'iris-valves--005--1x1', transform: [-1, 0, 0, 1, 960, 540] },
+    { symbol: 'console--031--1x1.2', transform: [-1, 0, 0, 1, 360, 60] },
+    { symbol: 'console--031--1x1.2', transform: [1, 0, 0, 1, 840, 60] },
   ],
 };
 
