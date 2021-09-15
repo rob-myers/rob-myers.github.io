d344250df21166170d5365179951de6dc9ae3817
Robert S.R. Myers <me.robmyers@gmail.com>
2021-09-15 20:04:18 +0100

    layout defs in geomorph/layout-defs

--- projects/geomorph/GeomorphDemo.jsx
+++ projects/geomorph/GeomorphDemo.jsx
@@ -6,6 +6,7 @@ import PanZoom from '../panzoom/PanZoom';
 import { createLayout, deserializeSvgJson, filterSingles } from "./geomorph.model";
 import { drawTriangulation, fillPolygon, fillRing } from '../service';
 import svgJson from '../../public/symbol/svg.json';
+import { layout301 } from "./layout-defs";
 
 export default function GeomorphDemo() {
   useQuery('focus-trigger', () => {}); // TODO only trigger in dev
@@ -67,32 +68,6 @@ function computeLayout(def) {
     ...layout,
   };
 }
-
-/** @type {Geomorph.LayoutDef} */
-const layout301 = {
-  key: 'g-301--bridge',
-  id: 301,
-  items: [
-    { symbol: '301--hull', tags: ['door']  }, // Hull must be first
-    { symbol: 'misc-stellar-cartography--023--4x4', transform: [-1, 0, 0, 1, 1200, 360] },
-    { symbol: 'stateroom--014--2x2', transform: [1, 0, 0, -1, 0, 480] },
-    { symbol: 'stateroom--014--2x2', transform: [1, 0, 0, -1, 120, 480] },
-    { symbol: 'office--001--2x2', transform: [-1, 0, 0, 1, 240, 120], tags: ['door-s'] },
-    { symbol: 'office--001--2x2', transform: [1, 0, 0, 1, 960, 120], tags: ['door-s'] },
-    { symbol: 'stateroom--036--2x4' },
-    { symbol: 'stateroom--036--2x4', transform: [-1, 0, 0, 1, 1200, 0] },
-    { symbol: 'stateroom--036--2x4', transform: [0, -1, 1, 0, 0, 600] },
-    { symbol: 'bridge--042--8x9', transform: [1, 0, 0, 1, 360, 60] },
-    { symbol: 'iris-valves--005--1x1', transform: [0, -1, 1, 0, 0, 360] },
-    { symbol: 'iris-valves--005--1x1', transform: [0, 1, 1, 0, 1140, 240] },
-    { symbol: 'iris-valves--005--1x1', transform: [-1, 0, 0, 1, 360, 540] },
-    { symbol: 'iris-valves--005--1x1', transform: [-1, 0, 0, 1, 960, 540] },
-    { symbol: 'console--031--1x1.2', transform: [-1, 0, 0, 1, 360, 60] },
-    { symbol: 'console--031--1x1.2', transform: [1, 0, 0, 1, 840, 60] },
-    { symbol: 'weaponry--013--1x2', transform: [-1, 0, 0, 1, 360, -60] },
-    { symbol: 'weaponry--013--1x2', transform: [1, 0, 0, 1, 840, -60] },
-  ],
-};
 
 /**
  * @param {Geomorph.Layout} layout
