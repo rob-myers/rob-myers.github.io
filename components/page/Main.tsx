4d358bd8dfdc26b6dd564f638b401321b5284e64
Robert S.R. Myers <me.robmyers@gmail.com>
2021-09-25 12:31:45 +0100

    remove main > section

--- components/page/Main.tsx
+++ components/page/Main.tsx
@@ -4,29 +4,19 @@ import Title from "./Title";
 export default function Main({ children }: React.PropsWithChildren<{}>) {
   return (
     <main className={rootCss}>
-      <section>
-        <Title />
-        {children}
-      </section>
+      <Title />
+      {children}
     </main>
   )
 }
 
 export const rootCss = css`
-  display: flex;
-  flex-direction: column;
-  padding-bottom: 2rem;
-  max-width: 1024px;  
-  margin: 0 auto;
-
-  > section {
-    padding: 48px 64px;
-    @media(max-width: 1024px) {
-      padding: 32px 64px;
-      margin: 0;
-    }
-    @media(max-width: 800px) {
-      padding: 0;
-    }
+  padding: 48px 64px;
+  @media(max-width: 1024px) {
+    padding: 32px 64px;
+    margin: 0;
+  }
+  @media(max-width: 800px) {
+    padding: 0;
   }
 `;
