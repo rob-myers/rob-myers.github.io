b2973652ef1f979fa47e459b302a5426697bceae
Robert S.R. Myers <me.robmyers@gmail.com>
2021-09-30 12:02:10 +0100

    rename articles

--- pages/draft.tsx
+++ pages/draft.tsx
@@ -4,8 +4,8 @@ import Article from 'components/page/Article';
 import objectiveMd from 'articles/objective.md';
 import constraintsMd from 'articles/constraints.md';
 import technologyMd from 'articles/technology.md';
-import techPt2Md from 'articles/techPt2.md';
-import techPt3Md from 'articles/techPt3.md';
+import techPt2Md from 'articles/tech-pt2.md';
+import techPt3Md from 'articles/tech-pt3.md';
 import geomorphsMd from 'articles/geomorphs.md';
 
 export default function DraftPage() {
