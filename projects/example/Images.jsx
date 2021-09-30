9b87942b15128ab9306af08251abc58242be3f76
Robert S.R. Myers <me.robmyers@gmail.com>
2021-09-30 10:22:03 +0100

    make image scrollable

--- projects/example/Images.jsx
+++ projects/example/Images.jsx
@@ -3,21 +3,24 @@ import { css } from 'goober';
 
 export function Gm301Debug() {
   return (
-    <picture className={classNames('scrollable', rootCss)}>
-      {/* <source media="(min-width: 1200px)" srcset="/pics/g-301--bridge.debug.x2.png" /> */}
-      <img
-        draggable={false}
-        src="/pics/g-301--bridge.debug.x1.png"
-        alt="Geomorph 301 (Debug)"
-        width={1212}
-        height={628}
-      />
-    </picture>
+    <div className={classNames('scrollable', rootCss)}>
+      <picture>
+        {/* <source media="(min-width: 1200px)" srcset="/pics/g-301--bridge.debug.x2.png" /> */}
+        <img
+          draggable={false}
+          src="/pics/g-301--bridge.debug.x1.png"
+          alt="Geomorph 301 (Debug)"
+          width={1212}
+          height={628}
+        />
+      </picture>
+    </div>
   );
 }
 
 const rootCss = css`
   overflow-x: scroll;
+  overflow-y: scroll;
   height: 100%;
   img {
     animation: fadein 2s;
