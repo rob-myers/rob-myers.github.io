bac66ae723f6475c803c7e0810901e178c647b94
Robert S.R. Myers <me.robmyers@gmail.com>
2021-10-02 13:28:36 +0100

    test page has test article

---
+++ articles/test.md
@@ -0,0 +1,13 @@
+## Test page
+
+<div
+    className="tabs"
+    enabled="true"
+    height="600"
+    tabs="[
+      { key: 'component', filepath: 'geomorph/GeomorphDemo' },
+      // { key: 'terminal', session: 'test' },
+      // { key: 'terminal', session: 'other' },
+    ]"
+>
+</div>
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,29 +1,11 @@
 import Article from 'components/page/Article';
 import Main from 'components/page/Main';
+import testMd from 'articles/test.md';
 
 export default function TestPage() {
   return (
     <Main>
-
-      <Article dateTime="2021-07-19">
-        {`
-
-## Test page
-
-<div
-    className="tabs"
-    enabled="true"
-    height="600"
-    tabs="[
-      { key: 'component', filepath: 'geomorph/GeomorphDemo' },
-      // { key: 'terminal', session: 'test' },
-      // { key: 'terminal', session: 'other' },
-    ]"
->
-</div>
-
-        `}
-      </Article>
+      <Article dateTime="2021-07-19" children={testMd} />
     </Main>
   );
 }
