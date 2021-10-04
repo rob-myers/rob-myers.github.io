0593e692da29d4e0e5bf01ed3dcb6b813ef2bf89
Robert S.R. Myers <me.robmyers@gmail.com>
2021-10-04 13:17:05 +0100

    iterate on SideNav

--- components/page/SideNav.tsx
+++ components/page/SideNav.tsx
@@ -1,9 +1,15 @@
-import { css } from "goober";
+import React from 'react';
+import { css } from 'goober';
+import classNames from 'classnames';
 
 export default function SideNav() {
+  const [open, setOpen] = React.useState(true);
 
   return (
-    <nav className={rootCss}>
+    <nav
+      className={classNames(rootCss, !open && 'closed')}
+      onClick={() => setOpen(x => !x)}
+    >
       sidenav
     </nav>
   );
@@ -11,5 +17,19 @@ export default function SideNav() {
 
 const rootCss = css`
   color: white;
-  background: black;
+  background: rgba(0, 0, 0, 0.9);
+  cursor: pointer;
+
+  position: fixed;
+  left: 0;
+  height: 100%;
+  z-index: 20;
+  width: 120px;
+
+  transition: left 500ms ease;
+  &.closed {
+    left: -80px;
+  }
+
+  padding: 16px;
 `;
