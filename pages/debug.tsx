ac8ba3959ab5e880fdb2a60be6005af554aeab0d
Robert S.R. Myers <me.robmyers@gmail.com>
2021-10-11 15:00:29 +0100

    import markdown separately into /debug and /test

--- pages/debug.tsx
+++ pages/debug.tsx
@@ -1,6 +1,24 @@
 import Main from 'components/page/Main';
 import Articles from 'components/page/Articles';
 
+import type { ArticleKey } from 'articles';
+
+import objective from 'articles/objective.md';
+import constraints from 'articles/constraints.md';
+import technology from 'articles/constraints.md';
+import techPart2 from 'articles/tech-2.md';
+import techPart3 from 'articles/tech-3.md';
+import geomorphs from 'articles/geomorphs.md';
+
+const markdown: Partial<Record<ArticleKey, string>> = {
+  objective,
+  constraints,
+  technology,
+  'tech-2': techPart2,
+  'tech-3': techPart3,
+  geomorphs,
+};
+
 export default function DraftPage() {
   return (
     <Main>
@@ -13,6 +31,7 @@ export default function DraftPage() {
           'tech-3',
           'geomorphs',
         ]}
+        markdown={markdown}
       />
     </Main>
   );
--- pages/test.tsx
+++ pages/test.tsx
@@ -1,10 +1,16 @@
 import Main from 'components/page/Main';
 import Articles from 'components/page/Articles';
+import type { ArticleKey } from 'articles';
+import test from 'articles/test.md';
+
+const markdown: Partial<Record<ArticleKey, string>> = {
+  test,
+};
 
 export default function TestPage() {
   return (
     <Main>
-      <Articles keys={['test']} />
+      <Articles keys={['test']} markdown={markdown} />
     </Main>
   );
 }
