518b462ead794a3b5182f9798feade56978af002
Robert S.R. Myers <me.robmyers@gmail.com>
2021-10-15 15:33:40 +0100

    restyle Next

--- components/page/Next.tsx
+++ components/page/Next.tsx
@@ -5,8 +5,12 @@ import Link from "./Link";
 export default function NextArticle(props: Props) {
   return props.article ? (
     <div className={nextArticleCss}>
-      <Link href={getArticleHref(props.article)} forward>
-        continue
+      <Link
+        href={getArticleHref(props.article)}
+        forward
+        title="Continue to next article"
+      >
+        Next
       </Link>
     </div>
   ) : null;
@@ -17,26 +21,17 @@ interface Props {
 }
 
 const nextArticleCss = css`
-  cursor: pointer;
-  font-family: sans-serif;
-  letter-spacing: 4px;
-  
   height: 64px;
-  display: flex;
+  font-size: 1.1rem;
   margin-top: -64px;
-  font-size: 1.3rem;
   @media(max-width: 800px) {
-    font-size: 1.1rem;
-    margin-top: 0px;
+    margin-top: 0;
   }
 
+  display: flex;
+  justify-content: center;
+  align-items: center;
   a {
-    color: #555;
-    width: 100%;
-    height: 100%;
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
+    color: #444;
   }
 `;
