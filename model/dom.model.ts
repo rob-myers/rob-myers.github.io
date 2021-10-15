27e29aa239cb90a55aa5b3073cd39fac5e4deb9f
Robert S.R. Myers <me.robmyers@gmail.com>
2021-10-15 22:46:12 +0100

    do not wait for scroll when already there or at end of page

--- model/dom.model.ts
+++ model/dom.model.ts
@@ -4,13 +4,20 @@ import debounce from 'debounce';
  * A promise which resolves when scrolling finishes at `targetScrollY`.
  */
 export async function scrollFinish(targetScrollY: number){
+  if (
+    Math.abs(window.pageYOffset - targetScrollY) <= 2
+    || window.pageYOffset >= maxScrollHeight() - 2
+    && targetScrollY >= maxScrollHeight()
+  ) {// Already there, or end of page and cannot scroll further
+    return;
+  }
   return new Promise<void>((resolve, reject) => {
     const onScroll = debounce((e: Event) => {
       if (
         Math.abs(window.pageYOffset - targetScrollY) >= 2
-        && Math.abs(maxScrollHeight() - window.pageYOffset) >= 5
+        && window.pageYOffset <= maxScrollHeight() - 2
       ) {// Missed target and not at max scroll
-        reject(new Error('Scroll cancelled'));
+        reject(new Error('Scroll failed'));
       } else {
         resolve();
       }
