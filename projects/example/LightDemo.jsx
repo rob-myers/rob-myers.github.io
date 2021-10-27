ec2ffb574cefeb108ed42891663ae1aabc8059c9
Robert S.R. Myers <me.robmyers@gmail.com>
2021-10-27 11:09:11 +0100

    work towards draggable light

--- projects/example/LightDemo.jsx
+++ projects/example/LightDemo.jsx
@@ -3,7 +3,7 @@ import { css } from "goober";
 import { useQuery } from "react-query";
 
 import { Poly, Rect, Vect } from "../geom";
-import { geom } from "../service";
+import { geom, getSvgPos } from "../service";
 import PanZoom from "../panzoom/PanZoom";
 import { gridBounds, initViewBox } from "./defaults";
 
@@ -39,17 +39,53 @@ export default function LightDemo(props) {
 /** @param {{ gm: Geomorph.GeomorphJson }} props */
 function Light({ gm }) {
 
+  // TODO move draggable node into self-contained component
+
+  const [state, setState] = React.useState(() => {
+    return {
+      position: new Vect(300, 300),
+      dragging: false,
+      /** @param {React.PointerEvent} e */
+      startDrag: (e) => {
+        e.stopPropagation();
+        state.dragging = true;
+        state.lineEl.style.display = 'inline';
+        /** @type {SVGSVGElement} */
+        const svg = (state.lineEl.ownerSVGElement);
+        svg.addEventListener('pointermove', state.onMove);
+        svg.addEventListener('pointerleave', state.endDrag);
+        svg.addEventListener('pointerup', state.endDrag);
+      },
+      /** @param {PointerEvent}  e */
+      onMove: (e) => {
+        if (state.dragging) {
+          const { x, y } = getSvgPos({ clientX: e.clientX, clientY: e.clientY, ownerSvg: /** @type {SVGSVGElement} */ (state.lineEl.ownerSVGElement), pointerId: null });
+          state.lineEl.setAttribute('x2', String(x));
+          state.lineEl.setAttribute('y2', String(y));
+        }
+      },
+      endDrag: () => {
+        state.dragging = false; // Mutate
+        state.lineEl.style.display = 'none';
+        /** @type {SVGSVGElement} */
+        const svg = (state.lineEl.ownerSVGElement);
+        svg.removeEventListener('pointermove', state.onMove);
+        svg.removeEventListener('pointerleave', state.endDrag);
+        svg.removeEventListener('pointerup', state.endDrag);
+      },
+      /** @type {SVGLineElement} */
+      lineEl: ({}),
+    };
+  });
+
   const light = useMemo(() => {
     const polys = (gm.walls.map(x => Poly.from(x)) || []);
     const triangs = polys.flatMap(poly => geom.triangulationToPolys(poly.fastTriangulate()));
-    const position = new Vect(300, 300);
-    const polygon = geom.lightPolygon(position, 800, triangs);
+    const polygon = geom.lightPolygon(state.position, 800, triangs);
     const { rect: bounds } = polygon;
-    return {
-      polygon,
-      sourceRatios: position.clone().sub(bounds).scale(1 / bounds.width, 1 / bounds.height),
-    };
-  }, []);
+    const sourceRatios = state.position.clone().sub(bounds).scale(1 / bounds.width, 1 / bounds.height);
+    return { polygon, sourceRatios };
+  }, [state.position]);
 
   return <>
     <defs>
@@ -68,9 +104,31 @@ function Light({ gm }) {
       fill={`url(#light-radial)`}
       d={light.polygon.svgPath}
     />
+    <line
+      ref={(el) => el && (state.lineEl = el)}
+      className="drag-indicator"
+      x1={state.position.x}
+      y1={state.position.y}
+    />
+    <circle
+      className="light"
+      cx={state.position.x}
+      cy={state.position.y}
+      r={20}
+      // TODO does PointerEvents polyfill work?
+      onPointerDown={state.startDrag}
+      onPointerUp={state.endDrag}
+    />
   </>;
 }
 
 const rootCss = css`
-  /* TODO */
+  .light {
+    fill: red;
+    cursor: pointer;
+  }
+  .drag-indicator {
+    stroke: black;
+    display: none;
+  }
 `;
