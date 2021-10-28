d7150f21c6223ceb9312e86948c3e9bf852b5440
Robert S.R. Myers <me.robmyers@gmail.com>
2021-10-28 17:46:13 +0100

    can drag light

--- projects/example/LightDemo.jsx
+++ projects/example/LightDemo.jsx
@@ -2,13 +2,14 @@ import React, { useMemo } from "react";
 import { css } from "goober";
 import { useQuery } from "react-query";
 
-import { Poly, Rect, Vect } from "../geom";
+import { Poly, Vect } from "../geom";
 import { geom, getSvgPos } from "../service";
 import PanZoom from "../panzoom/PanZoom";
 import { gridBounds, initViewBox } from "./defaults";
 
 /**
- * TODO movable light
+ * TODO
+ * - move draggable node into self-contained component
  */
 
 /** @param {{ layoutKey: Geomorph.LayoutKey }} props */
@@ -39,27 +40,39 @@ export default function LightDemo(props) {
 /** @param {{ gm: Geomorph.GeomorphJson }} props */
 function Light({ gm }) {
 
-  // TODO move draggable node into self-contained component
+  const [renderCount, setRenderCount] = React.useState(1);
 
   const [state, setState] = React.useState(() => {
     return {
       position: new Vect(300, 300),
+      target: new Vect(300, 300),
       dragging: false,
       /** @param {React.PointerEvent} e */
       startDrag: (e) => {
         e.stopPropagation();
         state.dragging = true;
         state.lineEl.style.display = 'inline';
+        state.target.copy(state.position);
+        ['x1', 'x2'].forEach(attr => state.lineEl.setAttribute(attr, String(state.position.x)));
+        ['y1', 'y2'].forEach(attr => state.lineEl.setAttribute(attr, String(state.position.y)));
         /** @type {SVGSVGElement} */
         const svg = (state.lineEl.ownerSVGElement);
         svg.addEventListener('pointermove', state.onMove);
         svg.addEventListener('pointerleave', state.endDrag);
-        svg.addEventListener('pointerup', state.endDrag);
+        svg.addEventListener('pointerup', state.applyDrag);
+        window.addEventListener('keydown', state.endDragOnEscape);
+        svg.style.cursor = 'grabbing';
       },
       /** @param {PointerEvent}  e */
       onMove: (e) => {
         if (state.dragging) {
-          const { x, y } = getSvgPos({ clientX: e.clientX, clientY: e.clientY, ownerSvg: /** @type {SVGSVGElement} */ (state.lineEl.ownerSVGElement), pointerId: null });
+          const { x, y } = getSvgPos({
+            clientX: e.clientX,
+            clientY: e.clientY,
+            ownerSvg: /** @type {SVGSVGElement} */ (state.lineEl.ownerSVGElement),
+            pointerId: null,
+          });
+          state.target.set(x, y);
           state.lineEl.setAttribute('x2', String(x));
           state.lineEl.setAttribute('y2', String(y));
         }
@@ -67,25 +80,39 @@ function Light({ gm }) {
       endDrag: () => {
         state.dragging = false; // Mutate
         state.lineEl.style.display = 'none';
+        state.lineEl.setAttribute('x2', /** @type {*} */ (state.lineEl.getAttribute('x1')));
+        state.lineEl.setAttribute('y2', /** @type {*} */ (state.lineEl.getAttribute('y1')));
         /** @type {SVGSVGElement} */
         const svg = (state.lineEl.ownerSVGElement);
         svg.removeEventListener('pointermove', state.onMove);
         svg.removeEventListener('pointerleave', state.endDrag);
-        svg.removeEventListener('pointerup', state.endDrag);
+        svg.removeEventListener('pointerup', state.applyDrag);
+        window.removeEventListener('keydown', state.endDragOnEscape);
+        svg.style.cursor = 'auto';
       },
+      applyDrag: () => {
+        state.endDrag();
+        state.position.copy(state.target);
+        state.lineEl.setAttribute('x1', String(state.target.x));
+        state.lineEl.setAttribute('y1', String(state.target.y));
+        setRenderCount(x => ++x);
+      },
+      /** @param {KeyboardEvent} e */
+      endDragOnEscape: (e) => void (e.key === 'Escape' && state.endDrag()),
       /** @type {SVGLineElement} */
       lineEl: ({}),
     };
   });
 
   const light = useMemo(() => {
-    const polys = (gm.walls.map(x => Poly.from(x)) || []);
+    const polys = gm.walls
+      .map(x => Poly.from(x));
     const triangs = polys.flatMap(poly => geom.triangulationToPolys(poly.fastTriangulate()));
-    const polygon = geom.lightPolygon(state.position, 800, triangs);
+    const polygon = geom.lightPolygon(state.position, 1000, triangs);
     const { rect: bounds } = polygon;
     const sourceRatios = state.position.clone().sub(bounds).scale(1 / bounds.width, 1 / bounds.height);
     return { polygon, sourceRatios };
-  }, [state.position]);
+  }, [state.position.x, state.position.y]);
 
   return <>
     <defs>
@@ -95,8 +122,8 @@ function Light({ gm }) {
         cy={`${100 * light.sourceRatios.y}%`}
         r="50%"
       >
-        <stop offset="0%" style={{ stopColor: 'rgba(255, 255, 230, 0.75)' }} />
-        <stop offset="50%" style={{ stopColor: 'rgba(230, 230, 230, 0.2)' }} />
+        <stop offset="0%" style={{ stopColor: 'rgba(255, 255, 225, 0.7)' }} />
+        <stop offset="40%" style={{ stopColor: 'rgba(200, 200, 200, 0.2)' }} />
         <stop offset="100%" style={{ stopColor: 'rgba(255, 200, 255, 0)' }} />
       </radialGradient>
     </defs>
@@ -107,28 +134,30 @@ function Light({ gm }) {
     <line
       ref={(el) => el && (state.lineEl = el)}
       className="drag-indicator"
-      x1={state.position.x}
-      y1={state.position.y}
     />
     <circle
       className="light"
       cx={state.position.x}
       cy={state.position.y}
-      r={20}
+      r={10}
       // TODO does PointerEvents polyfill work?
       onPointerDown={state.startDrag}
-      onPointerUp={state.endDrag}
+      onPointerUp={state.applyDrag}
     />
   </>;
 }
 
 const rootCss = css`
   .light {
-    fill: red;
+    fill: white;
+    stroke: black;
     cursor: pointer;
+    stroke-width: 2;
   }
   .drag-indicator {
     stroke: black;
     display: none;
+    stroke-width: 2.5;
+    user-select: none;
   }
 `;
