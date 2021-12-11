aa509101794bdaf2c51b5a2d6836e5862df2fea7
Robert S.R. Myers <me.robmyers@gmail.com>
2021-12-11 12:17:45 +0000

    work towards dev  ui for Triangle

--- projects/example/TriangleDev.jsx
+++ projects/example/TriangleDev.jsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { useQuery } from "react-query";
+import { styled } from "goober";
 import { geomorphJsonPath, geomorphPngPath } from "../geomorph/geomorph.model";
 import { Poly, Rect, Vect } from '../geom';
 import * as defaults from "./defaults";
@@ -16,47 +18,99 @@ import PanZoom from "../panzoom/PanZoom";
  */
 export default function TriangleDev(props) {
   
-  const { data } = useQuery(`triangle-dev-${props.layoutKey}`, async () => {
+  const [state, setState] = React.useState(() => ({
+    minArea: { disabled: true, area: 505 },
+    maxMinAngle: { disabled: true, angle: 80 },
+  }));
+
+  const { data: gmAux } = useQuery(`gm-aux-${props.layoutKey}`, async () => {
     /** @type {Geomorph.GeomorphJson} */
     const json = await fetch(geomorphJsonPath(props.layoutKey)).then(x => x.json());
     const navPoly = json.navPoly.map(x => Poly.from(x)).slice(0, 1);
-
-    const { vs: vsJson, tris: triIds } = /** @type {Geom.TriangulationJson} */ (await fetch('/api/dev/triangle', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ polys: navPoly.map(x => x.geoJson) }),
-    }).then(x => x.json()));
-
-    const vs = vsJson.map(Vect.from);
-    const tris = triIds.map(triple => /** @type {Triple<Geom.Vect>} */ (triple.map(id => vs[id])));
-
     return {
+      navPoly,
       pngRect: json.pngRect,
-      tris,
     };
+  });
+
+  const { data: triData } = useQuery(`tri-dev-${props.layoutKey}-${!!gmAux}-${JSON.stringify(state)}`, async () => {
+    if (gmAux) {
+      const { vs: vsJson, tris: triIds } = /** @type {Geom.TriangulationJson} */ (await fetch('/api/dev/triangle', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ polys: gmAux.navPoly.map(x => x.geoJson) }),
+      }).then(x => x.json()));
+  
+      const vs = vsJson.map(Vect.from);
+      const tris = triIds.map(triple => /** @type {Triple<Geom.Vect>} */ (triple.map(id => vs[id])));
+      return {
+        tris,
+      };
+    }
   }, {
-    // staleTime: Infinity,
+    staleTime: Infinity,
   });
 
   return (
     <PanZoom gridBounds={defaults.gridBounds} initViewBox={initViewBox} maxZoom={6}>
 
-      {data && <>
-        <image {...data.pngRect} className="geomorph" href={geomorphPngPath(props.layoutKey)} />
-
-        {data.tris.map((points) =>
-          <polygon
-            stroke="red"
-            fill="none"
-            strokeWidth={1}
-            className="navtri"
-            points={`${points}`}
-          />
-        )}
+      {gmAux && <>
+        <image {...gmAux.pngRect} className="geomorph" href={geomorphPngPath(props.layoutKey)} />
       </>}
+
+      {triData?.tris.map((points) =>
+        <polygon
+          stroke="red"
+          fill="none"
+          strokeWidth={1}
+          className="navtri"
+          points={`${points}`}
+        />
+      )}
+
+      <ForeignObject xmlns="http://www.w3.org/1999/xhtml">
+        <div className="min-area">
+          <input type="range" id="min-tri-area-range" min={10} max={1000} defaultValue={state.minArea.area}
+            disabled={state.minArea.disabled}
+            onChange={()  => {}}
+          />
+          <label htmlFor="min-tri-area-range" onClick={() => setState(x => { x.minArea.disabled = !x.minArea.disabled; return { ...x }; })}>
+            min area
+          </label>
+        </div>
+        <div className="maxmin-angle">
+          <input type="range" id="maxmin-tri-angle-range" min={20} max={140} defaultValue={state.maxMinAngle.angle}
+            disabled={state.maxMinAngle.disabled}
+          />
+          <label htmlFor="maxmin-tri-angle-range" onClick={() => setState(x => { x.maxMinAngle.disabled = !x.maxMinAngle.disabled; return { ...x }; })}>
+            maxmin angle
+          </label>
+        </div>
+      </ForeignObject>
 
     </PanZoom>
   );
 }
 
 const initViewBox = new Rect(200, 0, 600, 600);
+
+const ForeignObject = styled('foreignObject')`
+  background: #eee;
+  border: 1px solid #aaa;
+  font-size: 1rem;
+  padding: 8px;
+  width: 220px;
+  height: 100px;
+  x: -6px;
+  y: -106px;
+
+  div { display: flex; }
+  label { cursor: pointer; user-select: none; }
+
+  div.min-area {
+    input[type="range"] { width: 80px; margin-right: 8px; }
+  }
+  div.maxmin-angle {
+    input[type="range"] { width: 80px; margin-right: 8px; }
+  }
+`;
