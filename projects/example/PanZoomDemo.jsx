fc16cb018f1a5a0c9c0c555ea57dfd8c861264e9
Robert S.R. Myers <me.robmyers@gmail.com>
2021-12-24 09:47:44 +0000

    simplify PanZoomDemo code

--- projects/example/PanZoomDemo.jsx
+++ projects/example/PanZoomDemo.jsx
@@ -11,24 +11,20 @@ export default function PanZoomDemo() {
         drag to <strong>pan</strong>, scroll/pinch to <strong>zoom</strong>
       </p>
       <PanZoom
-        initViewBox={biggerInitViewBox}
+        initViewBox={defaults.initViewBox}
         gridBounds={defaults.gridBounds}
         maxZoom={5}
       >
         <image
           href="/geomorph/g-301--bridge.debug.png"
-          x={pngRect.x * 2}
-          y={pngRect.y * 2}
-          style={{ transform: "scale(0.5)" }}
+          {...pngRect}
         />
-        {/* <rect fill="red" x={0} y={0} width={20} height={20} /> */}
       </PanZoom>
     </div>
   );
 }
 
-const pngRect = new Rect(-6, -22, 1212, 628);
-const biggerInitViewBox = defaults.initViewBox.clone().outset(50);
+const pngRect = (new Rect(-6, -22, 1212, 628)).json;
 
 const rootCss = css`
   display: flex;
