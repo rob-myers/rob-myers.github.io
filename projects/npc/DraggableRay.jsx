e7f532c6082a30f988c6f753965426484aa703aa
Robert S.R. Myers <me.robmyers@gmail.com>
2022-01-21 12:20:58 +0000

    DraggableRay supports onClick and onStop

--- projects/npc/DraggableRay.jsx
+++ projects/npc/DraggableRay.jsx
@@ -3,6 +3,10 @@ import { css } from "goober";
 import { getSvgPos } from "../service/dom";
 import { Vect } from "../geom/vect";
 
+// TODO onStop provides target
+// TODO onClick provides target
+// TODO NPC turns towards drag point onStop
+
 /** @param {Props} props */
 export default function DraggableRay(props) {
 
@@ -40,23 +44,24 @@ export default function DraggableRay(props) {
         svg.addEventListener('pointerup', state.applyDrag);
         window.addEventListener('keydown', state.endDragOnEscape);
         svg.style.cursor = 'grabbing';
+        state.target.copy(state.source);
         props.onStart?.();
       },
       /** @param {PointerEvent}  e */
       onMove: (e) => {
         e.stopPropagation();
         if (state.dragging) {
-          const { x, y } = getSvgPos({
+          const target = getSvgPos({
             clientX: e.clientX,
             clientY: e.clientY,
             ownerSvg: /** @type {SVGSVGElement} */ (state.lineEl.ownerSVGElement),
             pointerId: null,
           });
-          const target = new Vect(x, y).sub(state.source);
-          if (target.length >= 20) {
-            state.target.copy(target);
-            state.lineEl.setAttribute('x2', String(target.x));
-            state.lineEl.setAttribute('y2', String(target.y));
+          state.target.copy(target);
+          const delta = Vect.from(target).sub(state.source);
+          if (delta.length >= 20) {
+            state.lineEl.setAttribute('x2', String(delta.x));
+            state.lineEl.setAttribute('y2', String(delta.y));
           }
         }
       },
@@ -76,15 +81,14 @@ export default function DraggableRay(props) {
       applyDrag: () => {
         if (!state.dragging) return;
         state.endDrag();
+        const distance = state.source.distanceTo(state.target);
 
-        if (props.shouldCancel?.(state.target)) {
-          return;
-        } else if (state.target.length < 20) {// Click 
-          console.log('click')
-          // props.onClick?.(state.position.clone());
-        } else {// Drag
-          console.log('drag', state.target.clone())
-          // props.onStop?.(state.target.clone()); // TODO
+        if (distance < 8) {// Click 
+          // console.log('click')
+          props.onClick?.(state.target.clone());
+        } else if (distance > 20) {// Drag
+          // console.log('drag')
+          props.onStop?.(state.target.clone());
         }
       },
       /** @param {KeyboardEvent} e */
@@ -110,14 +114,13 @@ export default function DraggableRay(props) {
     <g className={rootCss} ref={state.rootRef}>
       <circle
         className="drag-source"
-        // fill="blue"
         fill="rgba(0, 0, 0, 0.2)"
         r={props.radius}
       />
       <line
         className="drag-indicator"
-        stroke="blue"
-        strokeWidth={1}
+        stroke="#444"
+        strokeWidth={2}
       />
     </g>
   );
@@ -128,6 +131,9 @@ const rootCss = css`
   circle {
     cursor: crosshair;
   }
+  line {
+    pointer-events: none;
+  }
 `;
 
 /**
@@ -135,6 +141,6 @@ const rootCss = css`
  * @property {number} radius
  * @property {(api: NPC.DraggableRayApi) => void} [onLoad]
  * @property {() => void} [onStart]
- * @property {(angle: number) => void} [onStop]
- * @property {(target: Geom.Vect) => void} [shouldCancel]
+ * @property {(target: Geom.Vect) => void} [onStop]
+ * @property {(target: Geom.Vect) => void} [onClick]
  */
