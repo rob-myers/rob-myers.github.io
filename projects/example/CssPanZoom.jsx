edd51d92fc409429d1d0a4fe8ae3d2993f27a418
Robert S.R. Myers <me.robmyers@gmail.com>
2022-01-28 17:48:16 +0000

    CssPanZoom has same controls as PanZoom for desktop

--- projects/example/CssPanZoom.jsx
+++ projects/example/CssPanZoom.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { css } from 'goober';
 import useMeasure from 'react-use-measure';
-import { Rect } from '../geom';
+import { Rect, Vect } from '../geom';
 import useUpdate from '../hooks/use-update';
-import { getRelativePos } from '../service/dom';
+import { getSvgMid, getSvgPos, isSvgEvent, projectSvgEvt } from '../service/dom';
 
 /** @param {Props} props */
 export default function CssPanZoom(props) {
@@ -13,35 +13,77 @@ export default function CssPanZoom(props) {
 
   const [state] = React.useState(() => {
     /** @type {Context} */
-    const ctxt = {
+    const initialCtxt = {
       renderBounds: new Rect,
       zoomFactor: 100,
     };
 
     return {
       root: /** @type {SVGSVGElement} */ ({}),
-      ctxt,
+      ctxt: initialCtxt,
       gridId: `grid-${gridIdCount++}`,
-      Context: React.createContext(ctxt),
+      /** @type {null | Geom.Vect} */
+      panFrom: null,
+      /** @type {import('../service/dom').SvgPtr[]} */
+      ptrs: [],
+      /** @type {null | number} */
+      ptrDiff: null,
+      prevMouse: new Vect,
+      
+      Context: React.createContext(initialCtxt),
+      /** @param {PointerEvent} e */
+      onPointerDown: e => {
+        if (isSvgEvent(e) && state.ptrs.length < 2) {
+          state.panFrom = (new Vect).copy(getSvgPos(projectSvgEvt(e)));
+          state.ptrs.push(projectSvgEvt(e));
+          state.prevMouse.copy(state.panFrom);
+        }
+      },
+      /** @param {PointerEvent} e */
+      onPointerMove: e => {
+        state.ptrs = state.ptrs.map(x => x.pointerId === e.pointerId ? projectSvgEvt(e) : x);
 
+        if (state.ptrs.length === 2) {
+          const ptrDiff = Math.abs(state.ptrs[1].clientX - state.ptrs[0].clientX);
+          if (state.ptrDiff !== null) {
+            const point = getSvgMid(state.ptrs);
+            state.zoomTo(point, 0.02 * (ptrDiff - state.ptrDiff));
+            state.updateCtxt();
+            // props.onUpdate?.(state.root);
+          }          
+          state.ptrDiff = ptrDiff;
+        } else if (state.panFrom) {
+          const mouse = getSvgPos(projectSvgEvt(e));
+          state.ctxt.renderBounds.delta(
+            state.prevMouse.x - mouse.x,
+            state.prevMouse.y - mouse.y,
+          );
+          state.updateCtxt();
+          // props.onUpdate?.(state.root);
+          state.prevMouse.copy(mouse);
+        }
+      },
+      /** @param {PointerEvent} e */
+      onPointerUp: (e) => {
+        state.panFrom = null;
+        state.ptrs = state.ptrs.filter(alt => e.pointerId !== alt.pointerId);
+        if (state.ptrs.length < 2) {
+          state.ptrDiff = null;
+        }
+        if (state.ptrs.length === 1) {
+          state.panFrom = (new Vect).copy(getSvgPos(state.ptrs[0]));
+        }
+      },
       /** @type {(e: WheelEvent) => void} */
       onWheel(e) {
         e.preventDefault();
-        const ctxt = state.ctxt;
-        if (e.ctrlKey) {// Zoom
-          const nextZoom = ctxt.zoomFactor - 0.5 * e.deltaY;
-          if (Math.abs(e.deltaY) > 0.1 && nextZoom >= 25 && nextZoom <= 800) {
-            // Preserve world position of mouse
-            const { x: svgPosX, y: svgPosY } = getRelativePos(e)
-            ctxt.renderBounds.x += svgPosX * 100 * (1 / ctxt.zoomFactor - 1 / nextZoom);
-            ctxt.renderBounds.y += svgPosY * 100 * (1 / ctxt.zoomFactor - 1 / nextZoom);
+        if (isSvgEvent(e)) {
+          const point = getSvgPos(projectSvgEvt(e));
+          const nextZoom = state.ctxt.zoomFactor - wheelDelta * e.deltaY;
+          if (Math.abs(e.deltaY) > 0.1 && nextZoom >= 25 && nextZoom <= 800) { 
+            state.zoomTo(point, nextZoom);
             state.updateCtxt({ zoomFactor: nextZoom });
           }
-        } else {// Pan
-          // Fresh render bounds triggers update
-          const nextBounds = ctxt.renderBounds.clone()
-            .delta(0.5 * e.deltaX, 0.5 * e.deltaY);
-          state.updateCtxt({ renderBounds: nextBounds });
         }
       },
       /** @type {React.RefCallback<SVGSVGElement>} */
@@ -50,12 +92,25 @@ export default function CssPanZoom(props) {
         if (el) {
           state.root = el;
           el.addEventListener('wheel', state.onWheel);
+          el.addEventListener('pointerdown', state.onPointerDown, { passive: true });
+          el.addEventListener('pointermove', state.onPointerMove, { passive: true });
+          el.addEventListener('pointerup', state.onPointerUp, { passive: true });
         }
       },
       /** @param {Partial<Context>} [partial] */
       updateCtxt(partial) {
         state.ctxt = { ...state.ctxt, ...partial };
         update();
+      },
+      /**
+       * Zoom, preserving world position of mouse.
+       * @param {Geom.VectJson} point 
+       * @param {number} nextZoom
+       */
+      zoomTo(point, nextZoom) {
+        const { x: svgPosX, y: svgPosY } = point;
+        state.ctxt.renderBounds.x += svgPosX * 100 * (1 / state.ctxt.zoomFactor - 1 / nextZoom);
+        state.ctxt.renderBounds.y += svgPosY * 100 * (1 / state.ctxt.zoomFactor - 1 / nextZoom);
       },
     };
   });
@@ -81,7 +136,7 @@ export default function CssPanZoom(props) {
       preserveAspectRatio="xMinYMin slice"
     >
       <defs>
-        <pattern // Grid pattern.
+        <pattern // Grid pattern
           id={state.gridId}
           x={dx}
           y={dy}
@@ -99,18 +154,22 @@ export default function CssPanZoom(props) {
       </defs>
 
       <g transform={`scale(${scale})`}>
-
-        <rect // The grid
+        <rect
+          className="grid"
           width={`${100 / scale}%`}
           height={`${100 / scale}%`}
           fill={`url(#${state.gridId})`}
         />
+        <g transform={`translate(${-min.x}, ${-min.y})`}>
+          <state.Context.Provider value={state.ctxt}>
 
+            <rect fill="red" x="10" y="10" width="100" height="50" />
+
+            {/** Content here */}
+
+          </state.Context.Provider>
+        </g>
       </g>
-
-      <state.Context.Provider value={state.ctxt}>
-
-      </state.Context.Provider>
     </svg>
   ); 
 }
@@ -119,6 +178,10 @@ const rootCss = css`
   width: 100%;
   height: 100%;
   background: #888;
+
+  rect.grid {
+    pointer-events: none;
+  }
 `
 
 /**
@@ -131,5 +194,6 @@ const rootCss = css`
  * @property {number} zoomFactor
  */
 
+const wheelDelta = 0.3;
 const gridDim = 5;
 let gridIdCount = 0;
