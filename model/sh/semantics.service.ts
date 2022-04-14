9464c1d02545d3a204a3598c9d8cde98438e8ac9
Rob Myers <me.robmyers@gmail.com>
2022-04-14 12:52:15 +0100

    chore: stop suppressing `run` errors (confusing + bugs)

--- model/sh/semantics.service.ts
+++ model/sh/semantics.service.ts
@@ -273,11 +273,8 @@ class SemanticsService {
     } catch (e) {
       const command = node.type === 'CallExpr' ? node.Args[0].string || 'unknown CallExpr' : node.type;
       const error = e instanceof ShError ? e : new ShError('', 1, e as Error);
-      // Suppress `run` in subprocesses for cleaner error messages
-      if (node.meta.pid === 0 || (command !== 'run' && !node.meta.stack.includes('run'))) {
-        error.message = `${node.meta.stack.concat(command).join(': ')}: ${(e as Error).message || e}`;
-        sem.handleShError(node, e);
-      }
+      error.message = `${node.meta.stack.concat(command).join(': ')}: ${(e as Error).message || e}`;
+      sem.handleShError(node, e);
     }
   }
 
