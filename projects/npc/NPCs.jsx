080f513bfc23046a527fa7fb7d57414800d5922a
Rob Myers <me.robmyers@gmail.com>
2022-04-14 18:01:26 +0100

    chore: work towards NPCs

--- projects/npc/NPCs.jsx
+++ projects/npc/NPCs.jsx
@@ -1,4 +1,29 @@
+import React from "react";
+import classNames from "classnames";
+import { css } from "goober";
+import useStage from "../hooks/use-stage";
+
 /** @param {NPC.NPCsProps} props */
 export default function NPCs(props) {
-  return null;
+
+  const stage = useStage(props.stageKey);
+
+  return (
+    <div className={classNames('npcs', rootCss)}>
+      {
+        // TODO
+      }
+    </div>
+  );
 }
+
+const rootCss = css`
+  position: absolute;
+  canvas {
+    position: absolute;
+    pointer-events: none;
+  }
+  .npc {
+    position: absolute;
+  }
+`;
