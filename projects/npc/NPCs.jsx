46c5e41d4441c3d84b40fc5b708f32cab5bb08e4
Rob Myers <me.robmyers@gmail.com>
2022-05-02 11:50:15 +0100

    chore: create npc in service/npc

--- projects/npc/NPCs.jsx
+++ projects/npc/NPCs.jsx
@@ -11,6 +11,7 @@ import useGeomorphsNav from "../hooks/use-geomorphs-nav";
 
 // TODO further modularisation
 import npcJson from '../../public/npc/first-npc.json'
+import { createNpc } from "projects/service/npc";
 
 /** @param {NPC.NPCsProps} props */
 export default function NPCs(props) {
@@ -122,94 +123,13 @@ export default function NPCs(props) {
   React.useEffect(() => {
     const wire = ensureWire(props.wireKey);
 
+    // TODO 🚧 on HMR, refresh each npc in state.npc
+    console.log(state.npc);
+    // const oldNpcs = Object.values(state.npc);
+
     const sub = wire.subscribe((e) => {
       if (e.key === 'spawn') {
-        /** @type {NPC.NPC} */
-        const npc = {
-          key: e.npcKey,
-          uid: `${e.npcKey}-${++spawnCount}`,
-          def: {
-            key: e.npcKey,
-            position: e.at,
-            angle: Math.PI/4, // TEMP 🚧
-            paused: !!props.disabled,
-          },
-          el: {
-            root: /** @type {HTMLDivElement} */ ({}),
-            body: /** @type {HTMLDivElement} */ ({}),
-          },
-          anim: {
-            root: new Animation,
-            body: new Animation,
-          },
-          animPath: [],
-          aux: {
-            angs: [], count: 0, edges: [], elens: [], navPathPolys: [], sofars: [], total: 0,
-          },
-          origPath: [],
-          spriteSheet: 'idle',
-
-          followNavPath() {
-            const { aux } = this;
-            if (this.animPath.length <= 1 || aux.total === 0) {
-              return; // Already finished
-            }
-            
-            const wasPaused = this.anim.root.playState === 'paused';
-            this.anim.root = this.el.root.animate(
-              // NOTE need ≥ 2 frames for polyfill
-              this.animPath.flatMap((p, i) => [
-                {
-                  offset: aux.sofars[i] / aux.total,
-                  transform: `translate(${p.x}px, ${p.y}px) rotateZ(${aux.angs[i - 1] || aux.angs[i] || 0}rad)`,
-                },
-                {
-                  offset: aux.sofars[i] / aux.total,
-                  transform: `translate(${p.x}px, ${p.y}px) rotateZ(${aux.angs[i] || aux.angs[i - 1] || 0}rad)`,
-                },
-              ]),
-              { duration: aux.total * animScaleFactor, direction: 'normal', fill: 'forwards' },
-            );
-            
-            this.spriteSheet = 'walk';
-            this.anim.root.addEventListener('finish', () => { this.spriteSheet = 'idle'; update(); });
-
-            if (wasPaused || (aux.count === 0 && this.def.paused)) {
-              this.pause();
-            }
-            aux.count++;
-          },
-          getAngle() {
-            const matrix = new DOMMatrixReadOnly(window.getComputedStyle(this.el.root).transform);
-            return Math.atan2(matrix.m12, matrix.m11);
-          },
-          getPosition() {
-            const { x: clientX, y: clientY } = Vect.from(this.el.root.getBoundingClientRect());
-            return Vect.from(props.panZoomApi.getWorld({ clientX, clientY }));
-          },
-          pause() {
-            if (this.anim.body.playState === 'running') {
-              this.anim.body.pause();
-            }
-          },
-          updateAnimAux() {
-            const { animPath, aux } = this;
-            aux.edges = animPath.map((p, i) => ({ p, q: animPath[i + 1] })).slice(0, -1);
-            aux.angs = aux.edges.map(e => Number(Math.atan2(e.q.y - e.p.y, e.q.x - e.p.x).toFixed(2)));
-            aux.elens = aux.edges.map(({ p, q }) => Number(p.distanceTo(q).toFixed(2)));
-            aux.navPathPolys = aux.edges.map(e => {
-              const normal = e.q.clone().sub(e.p).rotate(Math.PI/2).normalize(0.01);
-              return new Poly([e.p.clone().add(normal), e.q.clone().add(normal), e.q.clone().sub(normal), e.p.clone().sub(normal)]);
-            });
-            const reduced = aux.elens.reduce((agg, length) => {
-              agg.total += length;
-              agg.sofars.push(agg.sofars[agg.sofars.length - 1] + length);
-              return agg;
-            }, { sofars: [0], total: 0 });
-            [aux.sofars, aux.total] = [reduced.sofars, reduced.total];
-          },
-        };
-        state.npc[e.npcKey] = npc;
+        state.npc[e.npcKey] = createNpc(e.npcKey, e.at, { panZoomApi: props.panZoomApi, update, disabled: props.disabled });
         update();
       } else if (e.key === 'nav-req') {
         const npc = state.npc[e.npcKey];
@@ -280,8 +200,6 @@ const { animLookup: anim, zoom } = npcJson;
 const npcScale = 0.18;
 /** Ensure NPC faces along positive x-axis */
 const npcOffsetAngleDeg = 90;
-/** Scale up how long it should take to move along navpath */
-const animScaleFactor = 20;
 const spriteSheets = keys(anim);
 
 const npcCss = css`
--- projects/service/npc.js
+++ projects/service/npc.js
@@ -2,6 +2,7 @@ import cheerio, { CheerioAPI, Element } from 'cheerio';
 import { Image, createCanvas, Canvas } from 'canvas';
 import path from 'path';
 
+import { Poly, Vect } from '../geom';
 import { extractGeomsAt, extractMetas, hasTitle } from './cheerio';
 import { saveCanvasAsFile } from './file';
 
@@ -105,3 +106,102 @@ function extractNpcFrames(api, topNodes, title) {
   for (const group of groups) output.push(extractMetas(api, group));
   return output;
 }
+
+/**
+ * TODO 🚧 deps needs rethink
+ * @param {string} npcKey 
+ * @param {Geom.VectJson} at 
+ * @param {{ disabled?: boolean; panZoomApi: PanZoom.CssExtApi; update: () => void }} deps
+ */
+export function createNpc(npcKey, at, {disabled, panZoomApi, update}) {
+  /** @type {NPC.NPC} */
+  const npc = {
+    key: npcKey,
+    uid: `${npcKey}-${++spawnCount}`,
+    def: {
+      key: npcKey,
+      position: at,
+      angle: 0, // TODO 🚧 integrate
+      paused: !!disabled,
+    },
+    el: {
+      root: /** @type {HTMLDivElement} */ ({}),
+      body: /** @type {HTMLDivElement} */ ({}),
+    },
+    anim: {
+      root: new Animation,
+      body: new Animation,
+    },
+    animPath: [],
+    aux: {
+      angs: [], count: 0, edges: [], elens: [], navPathPolys: [], sofars: [], total: 0,
+    },
+    origPath: [],
+    spriteSheet: 'idle',
+
+    followNavPath() {
+      const { aux } = this;
+      if (this.animPath.length <= 1 || aux.total === 0) {
+        return; // Already finished
+      }
+      
+      const wasPaused = this.anim.root.playState === 'paused';
+      this.anim.root = this.el.root.animate(
+        // NOTE need ≥ 2 frames for polyfill
+        this.animPath.flatMap((p, i) => [
+          {
+            offset: aux.sofars[i] / aux.total,
+            transform: `translate(${p.x}px, ${p.y}px) rotateZ(${aux.angs[i - 1] || aux.angs[i] || 0}rad)`,
+          },
+          {
+            offset: aux.sofars[i] / aux.total,
+            transform: `translate(${p.x}px, ${p.y}px) rotateZ(${aux.angs[i] || aux.angs[i - 1] || 0}rad)`,
+          },
+        ]),
+        { duration: aux.total * animScaleFactor, direction: 'normal', fill: 'forwards' },
+      );
+      
+      this.spriteSheet = 'walk';
+      this.anim.root.addEventListener('finish', () => { this.spriteSheet = 'idle'; update(); });
+
+      if (wasPaused || (aux.count === 0 && this.def.paused)) {
+        this.pause();
+      }
+      aux.count++;
+    },
+    getAngle() {
+      const matrix = new DOMMatrixReadOnly(window.getComputedStyle(this.el.root).transform);
+      return Math.atan2(matrix.m12, matrix.m11);
+    },
+    getPosition() {
+      const { x: clientX, y: clientY } = Vect.from(this.el.root.getBoundingClientRect());
+      return Vect.from(panZoomApi.getWorld({ clientX, clientY }));
+    },
+    pause() {
+      if (this.anim.body.playState === 'running') {
+        this.anim.body.pause();
+      }
+    },
+    updateAnimAux() {
+      const { animPath, aux } = this;
+      aux.edges = animPath.map((p, i) => ({ p, q: animPath[i + 1] })).slice(0, -1);
+      aux.angs = aux.edges.map(e => Number(Math.atan2(e.q.y - e.p.y, e.q.x - e.p.x).toFixed(2)));
+      aux.elens = aux.edges.map(({ p, q }) => Number(p.distanceTo(q).toFixed(2)));
+      aux.navPathPolys = aux.edges.map(e => {
+        const normal = e.q.clone().sub(e.p).rotate(Math.PI/2).normalize(0.01);
+        return new Poly([e.p.clone().add(normal), e.q.clone().add(normal), e.q.clone().sub(normal), e.p.clone().sub(normal)]);
+      });
+      const reduced = aux.elens.reduce((agg, length) => {
+        agg.total += length;
+        agg.sofars.push(agg.sofars[agg.sofars.length - 1] + length);
+        return agg;
+      }, { sofars: [0], total: 0 });
+      [aux.sofars, aux.total] = [reduced.sofars, reduced.total];
+    },
+  };
+  return npc;
+}
+
+let spawnCount = 0;
+/** Scale up how long it should take to move along navpath */
+const animScaleFactor = 15;
